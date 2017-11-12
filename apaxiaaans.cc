d07445ee307dea362d351dc8c018407723798d9a
kantuni <henrikh.kantuni@gmail.com>
2017-11-12 15:00:56 -0500

    Apaxiaaaaaaaaaaaans

---
+++ apaxiaaans.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string name;
+  cin >> name;
+  
+  string compact = string(1, name[0]);
+  for (int i = 1; i < name.size(); i++) {
+    if (name[i] != name[i - 1]) {
+      compact += string(1, name[i]);
+    }
+  }
+  
+  cout << compact << "\n";
+  return 0;
+}
