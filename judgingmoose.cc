df16c87593ae812b1a9bc49855402805a18db87f
kantuni <henrikh.kantuni@gmail.com>
2017-11-12 18:39:43 -0500

    Judging Moose

---
+++ judgingmoose.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int l, r;
+  cin >> l >> r;
+  
+  if (l == 0 && r == 0) {
+    cout << "Not a moose\n";
+  } else if (l == r) {
+    cout << "Even " << l + r << "\n";
+  } else {
+    cout << "Odd " << 2 * max(l, r) << "\n";
+  }
+  
+  return 0;
+}
