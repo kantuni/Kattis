7487ba7aa238c28a028874810ad3903af4f52775
kantuni <henrikh.kantuni@gmail.com>
2017-11-11 23:33:33 -0500

    Tarifa

---
+++ tarifa.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x;
+  cin >> x;
+  int n;
+  cin >> n;
+  
+  int ans = x;
+  while (n--) {
+    int p;
+    cin >> p;
+    ans += x - p;
+  }
+  
+  cout << ans << "\n";
+  return 0;
+}
