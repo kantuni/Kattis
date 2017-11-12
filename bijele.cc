768a5fed808c6d5a38aa3066dbf62cc2d955a254
kantuni <henrikh.kantuni@gmail.com>
2017-11-11 23:23:41 -0500

    Bijele

---
+++ bijele.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int correct[6] = {1, 1, 2, 2, 2, 8};
+  for (int i = 0; i < 6; i++) {
+    int n;
+    cin >> n;
+    cout << correct[i] - n;
+    if (i != 6) {
+      cout << " ";
+    }
+  }
+  cout << "\n";
+  return 0;
+}
