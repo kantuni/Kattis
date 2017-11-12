2640b4aa9d1b3910d90876c4eaee6105c406697b
kantuni <henrikh.kantuni@gmail.com>
2017-11-12 15:16:26 -0500

    FizzBuzz

---
+++ fizzbuzz.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y, n;
+  cin >> x >> y >> n;
+  for (int i = 1; i <= n; i++) {
+    if (i % x == 0 && i % y == 0) {
+      cout << "FizzBuzz\n";
+    } else if (i % x == 0) {
+      cout << "Fizz\n";
+    } else if (i % y == 0) {
+      cout << "Buzz\n";
+    } else {
+      cout << i << "\n";
+    }
+  }
+  return 0;
+}
