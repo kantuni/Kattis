dc42e4d897f9b1e1bc3013e05986dd57c3f8f444
kantuni <henrikh.kantuni@gmail.com>
2017-11-12 16:02:08 -0500

    Trik

---
+++ trik.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { 
+  string moves;
+  cin >> moves;
+  
+  int cups[3] = {1, 0, 0};
+  for (int i = 0; i < moves.size(); i++) {
+    if (moves[i] == 'A') {
+      swap(cups[0], cups[1]);
+    } else if (moves[i] == 'B') {
+      swap(cups[1], cups[2]);
+    } else {
+      swap(cups[0], cups[2]);
+    }
+  }
+  
+  for (int i = 0; i < 3; i++) {
+    if (cups[i] == 1) {
+      cout << i + 1 << "\n";
+    }
+  }
+  
+  return 0;
+}
