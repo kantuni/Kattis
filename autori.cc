039e58e9243f6c72cf2be7aa26c5fb56bbfdf331
kantuni <henrikh.kantuni@gmail.com>
2017-11-11 21:15:17 -0500

    Autori

---
+++ autori.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string l;
+  cin >> l;
+  
+  string abbr = string(1, l[0]);
+  for (int i = 1; i < l.size() - 1; i++) {
+    if (l[i] == '-') {
+      abbr += string(1, l[i + 1]);
+    }
+  }
+  
+  cout << abbr << "\n";
+  return 0;
+}
