d28e346fdc274ce6550e365c3f7921c268215571
kantuni <henrikh.kantuni@gmail.com>
2017-11-11 23:30:41 -0500

    No Duplicates

---
+++ no_duplicates.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  set<string> s;
+  string w;
+  int n = 0;
+  while (cin >> w) {
+    s.insert(w);
+    n++;
+  }
+  
+  if (s.size() == n) {
+    cout << "yes\n";
+  } else {
+    cout << "no\n";
+  }
+  
+  return 0;
+}
