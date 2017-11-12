e9b5cff4a0e2016dc759228b8bc7faaea7976c44
kantuni <henrikh.kantuni@gmail.com>
2017-11-11 22:50:17 -0500

    Quadrant Selection

---
+++ quadrant_selection.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x, y;
+  cin >> x >> y;
+  
+  int quad;
+  if (x > 0 && y > 0) {
+    quad = 1;
+  } else if (x > 0 && y < 0) {
+    quad = 4;
+  } else if (x < 0 && y > 0) {
+    quad = 2;
+  } else if (x < 0 && y < 0) {
+    quad = 3;
+  }
+  
+  cout << quad << "\n";
+  return 0;
+}
