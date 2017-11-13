288e71bfe026391ff0c5743d8a639165e66871f0
kantuni <henrikh.kantuni@gmail.com>
2017-11-13 11:04:36 -0500

    Death Knight Hero

---
+++ deathknight.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  int ans = 0;
+  while (n--) {
+    string s;
+    cin >> s;
+    
+    bool won = true;
+    for (int i = 0; i < s.size() - 1; i++) {
+      if (s[i] == 'C' && s[i + 1] == 'D') {
+        won = false;
+        break;
+      }
+    }
+    
+    if (won) {
+      ans++;
+    }
+  }
+  
+  cout << ans << "\n";
+  return 0;
+}
