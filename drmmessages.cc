51dfd9717e5553d1664f5e91c1c60a4d6d0f9e8c
kantuni <henrikh.kantuni@gmail.com>
2017-11-13 10:36:42 -0500

    DRM Messages

---
+++ drmmessages.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string drm;
+  cin >> drm;
+  
+  // 1
+  string l = drm.substr(0, drm.size() / 2);
+  string r = drm.substr(drm.size() / 2);
+  
+  // 2
+  int rvl = 0, rvr = 0;
+  for (int i = 0; i < drm.size() / 2; i++) {
+    rvl += l[i] - 'A';
+    rvr += r[i] - 'A';
+  }
+  
+  for (int i = 0; i < drm.size() / 2; i++) {
+    l[i] = (l[i] - 'A' + rvl) % 26 + 'A';
+    r[i] = (r[i] - 'A' + rvr) % 26 + 'A';
+  }
+  
+  // 3
+  for (int i = 0; i < drm.size() / 2; i++) {
+    l[i] = (l[i] - 'A' + r[i] - 'A') % 26 + 'A';
+  }
+  
+  cout << l << "\n";
+  return 0;
+}
