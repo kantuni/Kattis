9155a665ad1db043f50109a6d3c1ff805fd0b21a
kantuni <henrikh.kantuni@gmail.com>
2017-11-12 21:21:44 -0500

    Reversed Binary Numbers

---
+++ reversebinary.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  string bin;
+  while (n != 0) {
+    bin += to_string(n % 2);
+    n /= 2;
+  }
+  
+  int ans = 0;
+  for (int i = 0; i < bin.size(); i++) {
+    ans += (bin[bin.size() - 1 - i] - '0') * pow(2, i);
+  }
+  
+  cout << ans << "\n";
+  return 0;
+}
