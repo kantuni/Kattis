563ab954e1fb1fb6a7da3f20fb4a520a7a1a8325
kantuni <henrikh.kantuni@gmail.com>
2017-11-13 12:28:19 -0500

    Cryptographer's Conundrum

---
+++ conundrum.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  
+  int ans = 0;
+  for (int i = 0; i < s.size() - 2; i += 3) {
+    if (s[i] != 'P') {
+      ans++;
+    }
+    
+    if (s[i + 1] != 'E') {
+      ans++;
+    }
+    
+    if (s[i + 2] != 'R') {
+      ans++;
+    }
+  }
+  
+  cout << ans << "\n";
+  return 0;
+}
