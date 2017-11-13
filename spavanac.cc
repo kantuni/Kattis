5bd658411947c3e4666167746dc0567fa9c3d156
kantuni <henrikh.kantuni@gmail.com>
2017-11-12 20:59:26 -0500

    Spavanac

---
+++ spavanac.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int h, m;
+  cin >> h >> m;
+  
+  if (m > 45) {
+    cout << h << " " << m - 45 << "\n";
+  } else {
+    cout << (h - 1 + 24) % 24 << " " << (m - 45 + 60) % 60 << "\n";
+  }
+  
+  return 0;
+}
