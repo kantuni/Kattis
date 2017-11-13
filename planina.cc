3fa094c80fc0cd3a9d6cf9e1b06e2f415b8ab2f2
kantuni <henrikh.kantuni@gmail.com>
2017-11-12 21:51:44 -0500

    Planina

---
+++ planina.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  int ans = 2;
+  while (n--) {
+    ans = 2 * ans - 1;
+  }
+  
+  cout << ans * ans << "\n";
+  return 0;
+}
