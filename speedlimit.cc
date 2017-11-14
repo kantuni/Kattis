96edf339f509357b3e141278207102311240048c
kantuni <henrikh.kantuni@gmail.com>
2017-11-14 09:05:29 -0500

    Speed Limit

---
+++ speedlimit.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  while (true) {
+    int n;
+    cin >> n;
+    
+    if (n == -1) {
+      break;
+    }
+    
+    int prev = 0, ans = 0;
+    while (n--) {
+      int v, t;
+      cin >> v >> t;
+      ans += v * (t - prev);
+      prev = t;
+    }
+    cout << ans << " miles\n";
+  }
+  
+  return 0;
+}
