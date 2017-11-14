5a19c72e66d1da0498c633130ef4fa51286540a3
kantuni <henrikh.kantuni@gmail.com>
2017-11-14 08:39:47 -0500

    Stand on Zanzibar

---
+++ zanzibar.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  
+  while (t--) {
+    vector<int> num;
+    while (true) {
+      int n;
+      cin >> n;
+      if (n == 0) {
+        break;
+      }
+      num.push_back(n);
+    }
+    
+    int ans = 0;
+    for (int i = 0; i < num.size() - 1; i++) {
+      if (num[i + 1] > 2 * num[i]) {
+        ans += num[i + 1] - 2 * num[i];
+      }
+    }
+    cout << ans << "\n";
+  }
+  
+  return 0;
+}
