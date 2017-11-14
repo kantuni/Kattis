da2ef4620cd677fc1c4c144b0c271d184c2aaf11
kantuni <henrikh.kantuni@gmail.com>
2017-11-14 08:55:47 -0500

    Stacking Cups

---
+++ cups.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  map<int, string> cups;
+  while (n--) {
+    string s1, s2;
+    cin >> s1 >> s2;
+    
+    if (isdigit(s1[0])) {
+      int r = stoi(s1) / 2;
+      cups[r] = s2;
+    } else {
+      int r = stoi(s2);
+      cups[r] = s1; 
+    }
+  }
+  
+  for (auto c: cups) {
+    cout << c.second << "\n";
+  }
+  
+  return 0;
+}
