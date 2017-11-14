80eb8421f8404daaed55a7d6aff5b0a41a56d468
kantuni <henrikh.kantuni@gmail.com>
2017-11-13 21:51:31 -0500

    Hissing Microphone

---
+++ hissingmicrophone.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  
+  bool hiss = false;
+  for (int i = 0; i < s.size() - 1; i++) {
+    if (s[i] == 's' && s[i + 1] == 's') {
+      hiss = true;
+    }
+  }
+  
+  if (hiss) {
+    cout << "hiss\n";
+  } else {
+    cout << "no hiss\n";
+  }
+  
+  return 0;
+}
