a85a71ec2a7e6ab21cbd328d4d691e5f66c8fd4e
kantuni <henrikh.kantuni@gmail.com>
2017-11-15 17:06:37 -0500

    Detailed Differences

---
+++ detaileddifferences.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  for (int i = 0; i < n; i++) {
+    string a, b;
+    cin >> a >> b;
+    
+    cout << a << "\n";
+    cout << b << "\n";
+    for (int i = 0; i < a.size(); i++) {
+      cout << (a[i] == b[i] ? "." : "*");
+    }
+    cout << "\n\n";
+  }
+  
+  return 0;
+}
