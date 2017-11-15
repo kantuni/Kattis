a5eff094f7248dc02319bba760584d732114eef4
kantuni <henrikh.kantuni@gmail.com>
2017-11-15 16:52:20 -0500

    Herman

---
+++ herman.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI 3.14159265359
+
+int main() {
+  int r;
+  cin >> r;
+  cout << fixed << setprecision(6);
+  cout << PI * r * r << "\n";
+  cout << 2.0 * r * r << "\n";
+  return 0;
+}
