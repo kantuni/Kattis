bbf727d66dc92428f9b32ab3bd226263b92894c6
kantuni <henrikh.kantuni@gmail.com>
2017-11-15 08:52:39 -0500

    Kemija

---
+++ kemija08.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isvowel(char c) {
+  return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+}
+
+int main() {
+  int wc = 0;
+  string enc;
+  
+  while (cin >> enc) {
+    string dec;
+    for (int i = 0; i < enc.size(); i++) {
+      dec += enc[i];
+      if (isvowel(enc[i])) {
+        i += 2;
+      }
+    }
+    
+    if (wc != 0) cout << " ";
+    cout << dec;
+    wc++;
+  }
+  
+  return 0;
+}
