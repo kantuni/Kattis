94ff657be458d51303142e1a0e52a0e35fcdfce6
kantuni <henrikh.kantuni@gmail.com>
2017-11-15 07:47:08 -0500

    Odd Man Out

---
+++ oddmanout.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  for (int i = 0; i < n; i++) {
+    int g;
+    cin >> g;
+    
+    set<int> s;
+    for (int j = 0; j < g; j++) {
+      int c;
+      cin >> c;
+      
+      if (s.count(c) > 0) {
+        s.erase(c);
+      } else {
+        s.insert(c);
+      }
+    }
+    
+    cout << "Case #" << i + 1 << ": ";
+    cout << *s.begin() << "\n";
+  }
+  
+  return 0;
+}
