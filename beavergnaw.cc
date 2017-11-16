cfd25dfa19219e8e7808896e751fa3535e7bd11d
kantuni <henrikh.kantuni@gmail.com>
2017-11-16 11:06:51 -0500

    Beavergnaw

---
+++ beavergnaw.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI 3.14159265359
+#define EPS 1e-11
+
+int main() {
+  while (true) {
+    int D, V;
+    cin >> D >> V;
+    
+    if (D == 0 && V == 0) {
+      break;
+    }
+    
+    double Vt, Vsm, Vc, Vl;
+    double d, l, h;
+    
+    Vt = PI * D * D * D / 4.0;
+    Vt -= V;
+    
+    l = 0; h = D;
+    while (true) {
+      d = (l + h) / 2;
+      Vsm = PI * d * d * d / 4.0;
+      Vc = PI * (D - d) * (D * D + D * d + d * d) / 24.0;
+      Vl = Vsm + 2 * Vc;
+      
+      if (abs(Vt - Vl) < EPS) {
+        break;
+      } else if (Vt - Vl > EPS) {
+        l = d;
+      } else {
+        h = d;
+      }
+    }
+    printf("%.9f\n", d);
+  }
+  return 0;
+}
