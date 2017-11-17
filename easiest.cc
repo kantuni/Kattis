c44bf4668d02a07f4711177371f4a651b43980c0
kantuni <henrikh.kantuni@gmail.com>
2017-11-17 17:15:43 -0500

    The Easiest Problem Is This One

---
+++ easiest.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int dsum(int n) {
+  int sum = 0;
+  while (n != 0) {
+    sum += n % 10;
+    n /= 10;
+  }
+  return sum;
+}
+
+int main() {
+  while (true) {
+    int n;
+    cin >> n;
+    
+    if (n == 0) {
+      break;
+    }
+    
+    int p = 11;
+    while (dsum(p * n) != dsum(n)) {
+      p++;
+    }
+    cout << p << "\n";
+  }
+  return 0;
+}
