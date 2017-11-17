46563acdc5be274b09465e12959a33420ffe8b88
kantuni <henrikh.kantuni@gmail.com>
2017-11-17 17:22:35 -0500

    Soylent

---
+++ soylent.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    cout << ceil(n / 400.0) << "\n";
+  }
+  return 0;
+}
