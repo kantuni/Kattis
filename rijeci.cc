d75c1bdf0820c2f9dcebd92acd9da9662f86a7ae
kantuni <henrikh.kantuni@gmail.com>
2017-11-17 20:37:18 -0500

    Riječi

---
+++ rijeci.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int k;
+  cin >> k;
+  
+  int a = 0, b = 1;
+  while (k - 1 > 0) {
+    int tmp = b;
+    b = a + b;
+    a = tmp;
+    k--;
+  }
+  
+  cout << a << " " << b << "\n";
+  return 0;
+}
