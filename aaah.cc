cfcf25f64e4646b51dbb60fbcc96e4216148c56d
kantuni <henrikh.kantuni@gmail.com>
2017-11-18 14:37:13 -0500

    Aaah

---
+++ aaah.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string john, doc;
+  cin >> john >> doc;
+  cout << (john.size() >= doc.size() ? "go" : "no") << "\n";
+  return 0;
+}
