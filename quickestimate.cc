02ad78abe2ab0f3867e10201a8ecb530214a8779
kantuni <henrikh.kantuni@gmail.com>
2017-11-18 00:22:20 -0500

    Quick Estimates

---
+++ quickestimate.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  while (n--) {
+    string s;
+    cin >> s;
+    cout << s.size() << "\n";
+  }
+  return 0;
+}
