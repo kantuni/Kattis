bdd223f3b584fb0854943d7a28224fcb94480c6c
kantuni <henrikh.kantuni@gmail.com>
2017-11-18 00:32:01 -0500

    Parking

---
+++ parking2.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> pos(n);
+    for (int i = 0; i < n; i++) {
+      cin >> pos[i];
+    }
+    sort(pos.begin(), pos.end());
+    cout << 2 * (pos[pos.size() - 1] - pos[0]) << "\n";
+  }
+  return 0;
+}
