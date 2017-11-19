ee87d147dd1c15976e778a91e1a63e7e46576f22
kantuni <henrikh.kantuni@gmail.com>
2017-11-19 15:07:58 -0500

    Odd Gnome

---
+++ oddgnome.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  while (n--) {
+    int g;
+    cin >> g;
+    vector<int> gnomes(g);
+    for (int i = 0; i < g; i++) {
+      cin >> gnomes[i];
+    }
+    
+    for (int i = 1; i < g; i++) {
+      if (gnomes[i] != gnomes[i - 1] + 1) {
+        cout << i + 1 << "\n";
+        break;
+      }
+    }
+  }
+  return 0;
+}
