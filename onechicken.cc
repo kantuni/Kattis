f54a961b7488d75297477c4bc57d762d2c39cc18
kantuni <henrikh.kantuni@gmail.com>
2017-11-19 14:51:47 -0500

    One Chicken Per Person

---
+++ onechicken.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  
+  if (m > n) {
+    cout << "Dr. Chaz will have ";
+    cout << (m - n == 1 ? "1 piece" : to_string(m - n) + " pieces");
+    cout << " of chicken left over!\n";
+  } else {
+    cout << "Dr. Chaz needs ";
+    cout << (n - m == 1 ? "1 more piece" : to_string(n - m) + " more pieces");
+    cout << " of chicken!\n";
+  }
+  
+  return 0;
+}
