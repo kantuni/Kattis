6eb0cb40708b632aa37aa2ffbb773842b2f788e7
kantuni <henrikh.kantuni@gmail.com>
2017-11-20 15:51:20 -0500

    Dice Game - WA

---
+++ dicegame.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a1, a2, a3, a4;
+  int b1, b2, b3, b4;
+  cin >> a1 >> b1 >> a2 >> b2;
+  cin >> a3 >> b3 >> a4 >> b4;
+  
+  int gmin = a1 + a2;
+  int gmax = b1 + b2;
+  int emin = a3 + a4;
+  int emax = b3 + b4;
+  
+  if (gmin > emax) {
+    cout << "Gunnar\n";
+  } else if (emin > gmax) {
+    cout << "Emma\n";
+  } else {
+    int l = max(gmin, emin) - min(gmin, emin);
+    int r = max(gmax, emax) - min(gmax, emax);
+    
+    if (r == l) {
+      cout << "Tie\n";
+    } else if (r > l) {
+      cout << (max(gmax, emax) == gmax ? "Gunnar\n" : "Emma\n");
+    } else {
+      cout << (min(gmin, emin) == emin ? "Gunnar\n" : "Emma\n");
+    }
+  }
+  
+  return 0;
+}
