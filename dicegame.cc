c6bfbbcfa5475caf64f1473128890c449b00b4ec
kantuni <henrikh.kantuni@gmail.com>
2017-11-20 19:57:03 -0500

    Dice Game

--- dicegame.cc
+++ dicegame.cc
@@ -7,7 +7,6 @@ int main() {
   cin >> a1 >> b1 >> a2 >> b2;
   cin >> a3 >> b3 >> a4 >> b4;
   
-<<<<<<< HEAD
   int gmin = a1 + a2;
   int gmax = b1 + b2;
   int emin = a3 + a4;
@@ -26,28 +25,7 @@ int main() {
     } else if (r > l) {
       cout << (max(gmax, emax) == gmax ? "Gunnar\n" : "Emma\n");
     } else {
-      cout << (min(gmin, emin) == emin ? "Gunnar\n" : "Emma\n");
-=======
-  int ea = a1 + a2;
-  int eb = b1 + b2;
-  int ga = a3 + a4;
-  int gb = b3 + b4;
-  
-  if (ea - ga == gb - eb) {
-    cout << "Tie\n";
-  } else if (ga > eb) {
-    cout << "Gunnar\n";
-  } else if (ea > gb) {
-    cout << "Emma\n";
-  } else {
-    int l = max(ga, ea) - min(ga, ea);
-    int r = max(gb, eb) - min(gb, eb);
-    
-    if (r > l) {
-      cout << (max(gb, eb) == eb ? "Gunnar\n" : "Emma\n");
-    } else {
-      cout << (max(gb, eb) == eb ? "Emma\n" : "Gunnar\n");
->>>>>>> 6b8370f60e840a051fc9fb57e742831feaddf7f8
+      cout << (min(gmin, emin) == gmin ? "Emma\n" : "Gunnar\n");
     }
   }
   
