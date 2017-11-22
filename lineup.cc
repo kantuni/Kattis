08a859cef11a5e695e6ee5177ed162a42b1d47c2
kantuni <henrikh.kantuni@gmail.com>
2017-11-22 06:01:16 -0500

    Line Them Up

---
+++ lineup.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  
+  vector<string> names(n);
+  for (int i = 0; i < n; i++) {
+    cin >> names[i];
+  }
+  
+  vector<string> inc(names);
+  sort(inc.begin(), inc.end());
+  
+  vector<string> dec(names);
+  sort(dec.rbegin(), dec.rend());
+  
+  if (names == inc) {
+    cout << "INCREASING\n";
+  } else if (names == dec) {
+    cout << "DECREASING\n";
+  } else {
+    cout << "NEITHER\n";
+  }
+  
+  return 0;
+}
