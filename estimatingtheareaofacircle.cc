2e00e13b92a21ef55c024133ae9e5ce1ddbc42cb
kantuni <henrikh.kantuni@gmail.com>
2017-11-22 07:57:02 -0500

    Estimating the Area of a Circle

---
+++ estimatingtheareaofacircle.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  double r;
+  int m, c;
+  
+  while (cin >> r >> m >> c) {
+    if (r == 0 && m == 0 && c == 0) {
+      break;
+    }
+    
+    double carea = acos(-1.0) * r * r;
+    double sarea = 4 * r * r;
+    printf("%.7f %.7f\n", carea, sarea * c / m);
+  }
+  
+  return 0;
+}
