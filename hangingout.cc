7785bd4f941ef146e63ce15ae924c032ef263448
kantuni <henrikh.kantuni@gmail.com>
2017-11-23 07:56:37 -0500

    Hanging Out on the Terrace

---
+++ hangingout.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int l, x;
+  cin >> l >> x;
+  int curr = 0, ans = 0;
+  while (x--) {
+    string s;
+    int p;
+    cin >> s >> p;
+    if (s == "enter") {
+      if (curr + p <= l) {
+        curr += p;
+      } else {
+        ans++;
+      }
+    } else {
+      curr -= p;
+    }
+  }
+  cout << ans << "\n";
+  return 0;
+}
