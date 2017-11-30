8bbcc6953376cff717964c23ee0704472289f44a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2017-11-30 16:27:17 -0500

    Update cups.cc

--- cups.cc
+++ cups.cc
@@ -4,10 +4,12 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
+  
   map<int, string> cups;
   while (n--) {
     string s1, s2;
     cin >> s1 >> s2;
+    
     if (isdigit(s1[0])) {
       int r = stoi(s1) / 2;
       cups[r] = s2;
@@ -16,6 +18,7 @@ int main() {
       cups[r] = s1; 
     }
   }
+  
   for (auto c: cups) {
     cout << c.second << "\n";
   }
