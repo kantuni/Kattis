5e56bbac348ebce4967bd536e7a005aaa2a03118
kantuni <henrikh.kantuni@gmail.com>
2018-01-06 15:19:41 +0400

    ACM Contest Scoring

---
+++ acm.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Problem {
+  bool v = false;
+  int w = 0;
+  int m;
+};
+
+int main() {
+  unordered_map<char, Problem> memo;
+  int m; char l; string v;
+  
+  while (true) {
+    cin >> m;
+    
+    if (m == -1) {
+      break;
+    }
+    
+    cin >> l >> v;
+    
+    Problem p;
+    if (memo.count(l)) {
+      p = memo[l];
+    }
+    
+    if (v == "right") {
+      p.m = m;
+      p.v = true;
+    } else if (!p.v) {
+      p.m = m;
+      p.w++;
+    }
+    
+    memo[l] = p;
+  }
+  
+  int solved = 0, total = 0;
+  for (auto kv: memo) {
+    if (kv.second.v) {
+      total += kv.second.m + (20 * kv.second.w);
+      solved++;
+    }
+  }
+  
+  cout << solved << " " << total << "\n";
+  return 0;
+}
