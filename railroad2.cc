c13a3f45c3d876f661158ddeebe13c8d96964412
kantuni <henrikh.kantuni@gmail.com>
2018-01-08 19:19:31 +0400

    Railroad - WA

--- railroad2.cc
+++ railroad2.cc
@@ -5,12 +5,10 @@ int main() {
   int x, y;
   cin >> x >> y;
   
-  if (x == 0 && (y == 0 || y == 2)) {
-    cout << "possible\n";
-  } else if (x == 1 && (y == 0 || y == 4)) {
-    cout << "possible\n";
-  } else {
+  if (x % 2 == 1 && y % 2 == 1) {
     cout << "impossible\n";
+  } else {
+    cout << "possible\n";
   }
   
   return 0;
