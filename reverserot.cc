7984b089873340374b6e168e2f63c3457d1e3c86
kantuni <henrikh.kantuni@gmail.com>
2018-01-14 20:35:33 +0400

    Reverse Rot

---
+++ reverserot.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string encrypt(string m, int n) {
+  string sigma = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_.";
+  reverse(m.begin(), m.end());
+  
+  for (char &l: m) {
+    int index = sigma.find(l);
+    l = sigma[(index + n) % sigma.size()];
+  }
+  
+  return m;
+}
+
+int main() {
+  while (true) {
+    int n;
+    cin >> n;
+    
+    if (n == 0) {
+      break;
+    }
+    
+    string m;
+    cin >> m;
+    cout << encrypt(m, n) << "\n";
+  }
+  
+  return 0;
+}
