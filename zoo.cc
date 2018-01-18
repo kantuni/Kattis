1afcd50e7effbfeb2f9063d714b7eda3967894bc
kantuni <henrikh.kantuni@gmail.com>
2018-01-19 00:51:22 +0400

    Un-bear-able Zoo

--- zoo.cc
+++ zoo.cc
@@ -1,6 +1,38 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+  int ln = 1;
+  while (true) {
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    if (n == 0) {
+      break;
+    }
+
+    map<string, int> memo;
+
+    while (n--) {
+      string line;
+      getline(cin, line);
+      
+      istringstream iss(line);
+      string animal;
+      while (iss >> animal);
+
+      transform(animal.begin(), animal.end(), animal.begin(), ::tolower);
+      memo[animal] += 1;
+    }
+
+    cout << "List " << ln << ":\n";
+    for (auto m: memo) {
+      cout << m.first << " | " << m.second << "\n";
+    }
+
+    ln += 1;
+  }
+
   return 0;
 }
