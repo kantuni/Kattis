532f195693f1de013838db34a3b37da5fb5ca101
kantuni <henrikh.kantuni@gmail.com>
2018-01-19 17:16:14 +0400

    Update greedilyincreasing.cc

--- greedilyincreasing.cc
+++ greedilyincreasing.cc
@@ -5,9 +5,7 @@ int main() {
   int n;
   cin >> n;
 
-  vector<int> a(n);
-  map<int, int> asub;
-
+  vector<int> a(n), asub(n - 1);
   for (int i = 0; i < n; i++) {
     cin >> a[i];
     if (i != 0) {
@@ -18,10 +16,9 @@ int main() {
   vector<int> gis;
   gis.push_back(a[0]);
 
-  int curr = 0;
-  for (auto kv: asub) {
-    if (kv.first >= curr && kv.second > gis[gis.size() - 1]) {
-      gis.push_back(kv.second);
+  for (int i = 0; i < n - 1; i++) {
+    if (asub[i] > gis[gis.size() - 1]) {
+      gis.push_back(asub[i]);
     }
   }
 
