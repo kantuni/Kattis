297a27f0589393a4b679c868014b7633a54b848b
kantuni <henrikh.kantuni@gmail.com>
2018-01-29 21:33:43 +0400

    Saving For Retirement - AC

--- savingforretirement.cc
+++ savingforretirement.cc
@@ -4,7 +4,11 @@ using namespace std;
 int main() {
   int b, br, bs, a, as;
   cin >> b >> br >> bs >> a >> as;
-  double x = 1.0 * (br - b) * bs / as + a;
-  cout << ceil(x) << "\n";
+  double x = ceil(1.0 * (br - b) * bs / as + a);
+  bool ok = (x - a) * as > (br - b) * bs;
+  if (!ok) {
+    x++;
+  }
+  cout << x << "\n";
   return 0;
 }
