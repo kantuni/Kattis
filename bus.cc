d7fc2e8b8e52d7175d04344536fc6f90f8767106
kantuni <henrikh.kantuni@gmail.com>
2018-02-01 23:54:55 +0400

    Bus - AC

--- bus.cc
+++ bus.cc
@@ -1,29 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double iszero(double n, int k) {
-  while (k--) {
-    n = n - (n / 2.0) - 0.5;
-  }
-  return n;
-}
-
 int main() {
   int t;
   cin >> t;
   while (t--) {
     int k;
     cin >> k;
-    double l = 0, h = 100, m;
-    while (true) {
-      m = (l + h) / 2;
-      if (iszero(m, k) < 1e-7) break;
-      
-      if (iszero(l, k) > iszero(h, k)) {
-        l = m;
-      } else h = m;
-    }
-    cout << ceil(m) << "\n";
+    cout << (int) pow(2, k) - 1 << "\n";
   }
   return 0;
 }
