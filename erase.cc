e06e64340cb8fd1c6f75f85290beb8cc51c419b8
kantuni <henrikh.kantuni@gmail.com>
2018-02-02 18:38:03 +0400

    Erase Securely

---
+++ erase.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  string before, after;
+  cin >> before >> after;
+  if (n % 2 == 1) {
+    for (char &bit: after) {
+      bit = (bit == '0') ? '1' : '0';
+    }
+  }
+  bool failed = false;
+  for (int i = 0; i < before.size(); i++) {
+    if (before[i] != after[i]) {
+      failed = true;
+    }
+  }
+  
+  if (failed) {
+    cout << "Deletion failed\n";
+  } else {
+    cout << "Deletion succeeded\n";
+  }
+  return 0;
+}
