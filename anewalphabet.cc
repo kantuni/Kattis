359189c225a54c88e721c947106e12e0260fbf46
kantuni <henrikh.kantuni@gmail.com>
2018-02-07 18:21:49 +0400

    A New Alphabet

---
+++ anewalphabet.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  vector<string> alphabet = {
+    "@", "8", "(", "|)", "3", "#", "6",
+    "[-]", "|", "_|", "|<", "1", "[]\\/[]",
+    "[]\\[]", "0", "|D", "(,)", "|Z", "$",
+    "']['", "|_|", "\\/", "\\/\\/", "}{", "`/", "2"
+  };
+  string line;
+  getline(cin, line);
+  for (char l: line) {
+    if (isalpha(l)) {
+      cout << alphabet[toupper(l) - 'A'];
+    } else cout << l;
+  }
+  cout << "\n";
+  return 0;
+}
