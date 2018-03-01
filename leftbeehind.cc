875fb916595cf78d57e2d557c419e5a8ca0d0668
kantuni <henrikh.kantuni@gmail.com>
2018-03-01 15:20:33 -0500

    Left Beehind

---
+++ leftbeehind.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  while (true) {
+    int x, y;
+    cin >> x >> y;
+    if (x == 0 and y == 0) break;
+    if (x + y == 13) cout << "Never speak again.\n";
+    else if (y > x) cout << "Left beehind.\n";
+    else if (x > y) cout << "To the convention.\n";
+    else cout << "Undecided.\n";
+  }
+  return 0;
+}
