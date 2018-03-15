1f9ff82c248df9c85dc7007ecb0c1104cc8ced82
kantuni <henrikh.kantuni@gmail.com>
2018-03-15 08:09:10 -0400

    Simon Says

---
+++ simon.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  cin.ignore();
+  while (t--) {
+    string line;
+    getline(cin, line);
+    if (line.size() > 10 and line.substr(0, 10) == "simon says") {
+      cout << line.substr(11) << "\n";
+    } else {
+      cout << "\n";
+    }
+  }
+  return 0;
+}
