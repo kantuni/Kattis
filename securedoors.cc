36d9239fd9b3272607f29055fbb6ff723b5c1c3b
kantuni <henrikh.kantuni@gmail.com>
2018-03-16 10:23:20 -0400

    Secure Doors

---
+++ securedoors.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  cin.ignore();
+  unordered_map<string, bool> inside;
+  while (n--) {
+    string action, name;
+    cin >> action >> name;
+    if (!inside.count(name)) inside[name] = false;
+    cout << name;
+    if (action == "entry") {
+      cout << " entered";
+      cout << (inside[name] ? " (ANOMALY)\n" : "\n");
+      inside[name] = true;
+    } else {
+      cout << " exited";
+      cout << (!inside[name] ? " (ANOMALY)\n" : "\n");
+      inside[name] = false;
+    }
+  }
+  return 0;
+}
