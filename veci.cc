50c5d9820e76db6cca5a542e4a52af8b10bd59d8
kantuni <henrikh.kantuni@gmail.com>
2018-03-17 17:03:35 -0400

    Veci

---
+++ veci.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string x;
+  cin >> x;
+  int xn = stoi(x);
+  sort(x.begin(), x.end());
+  int sm = 1e7;
+  do {
+    int xp = stoi(x);
+    if (xp > xn and xp < sm) sm = xp;
+  } while (next_permutation(x.begin(), x.end()));
+  cout << (sm < 1e7 ? sm : 0) << "\n";
+  return 0;
+}
