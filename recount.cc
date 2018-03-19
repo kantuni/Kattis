93d32ce4c00fde9f0ead38bfc38524629b3f3b9e
kantuni <henrikh.kantuni@gmail.com>
2018-03-19 15:36:49 -0400

    Recount

---
+++ recount.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  unordered_map<string, int> memo;
+  while (1) {
+    string vote;
+    getline(cin, vote);
+    if (vote == "***") break;
+    memo[vote]++;
+  }
+  vector<int> votes;
+  for (auto m: memo) {
+    votes.push_back(m.second);
+  }
+  sort(votes.rbegin(), votes.rend());
+  if (votes[0] == votes[1]) {
+    cout << "Runoff!\n";
+  } else {
+    for (auto m: memo) {
+      if (m.second == votes[0]) {
+        cout << m.first << "\n";
+        break;
+      }
+    }
+  }
+  return 0;
+}
