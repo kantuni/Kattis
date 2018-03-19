d6f585a7543903463eb82d008a94f6bab488e5fb
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-19 15:52:14 -0400

    Update cetvrta.cc

--- cetvrta.cc
+++ cetvrta.cc
@@ -1,44 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef pair<int, int> ii;
-typedef vector<ii> vii;
-
 int main() {
-  vii coords;
+  vector<pair<int, int> > coords;
   for (int i = 0; i < 3; i++) {
     int x, y;
     cin >> x >> y;
-    coords.push_back(ii(x, y));
+    coords.push_back(make_pair(x, y));
   }
-  
   // (x1, y1), (x1, y2), (x2, y1), (x2, y2)
   // where x1 ≥ x2, y1 ≥ y2
   sort(coords.begin(), coords.end());
-  
   int dx, dy;
-  ii ans;
-  
+  pair<int, int> ans;
   if (coords[0].first == coords[1].first) {
     dx = coords[2].first - coords[0].first;
     dy = coords[1].second - coords[0].second;
-    
     if (coords[2].second == coords[0].second) {
-      ans = ii(coords[0].first + dx, coords[0].second + dy);
+      ans = make_pair(coords[0].first + dx, coords[0].second + dy);
     } else {
-      ans = ii(coords[0].first + dx, coords[0].second);
+      ans = make_pair(coords[0].first + dx, coords[0].second);
     }
   } else {
     dx = coords[2].first - coords[0].first;
     dy = coords[2].second - coords[1].second;
-    
     if (coords[2].second == coords[0].second) {
-      ans = ii(coords[2].first - dx, coords[2].second - dy);
+      ans = make_pair(coords[2].first - dx, coords[2].second - dy);
     } else {
-      ans = ii(coords[2].first - dx, coords[2].second);
+      ans = make_pair(coords[2].first - dx, coords[2].second);
     }
   }
-  
   cout << ans.first << " " << ans.second << "\n";
   return 0;
 }
