4ad2f0d6e2e9fce4b9691bd906ce80ee911d059e
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-19 15:58:25 -0400

    Update cold.cc

--- cold.cc
+++ cold.cc
@@ -4,16 +4,12 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  
   int ans = 0;
   while (n--) {
     int temp;
     cin >> temp;
-    if (temp < 0) {
-      ans++;
-    }
+    if (temp < 0) ans++;
   }
-  
   cout << ans << "\n";
   return 0;
 }
