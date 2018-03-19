ca89a02c8fd1d1666d42d0c32d8584d528ea2808
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-19 15:41:58 -0400

    Update beavergnaw.cc

--- beavergnaw.cc
+++ beavergnaw.cc
@@ -5,34 +5,23 @@ using namespace std;
 #define EPS 1e-11
 
 int main() {
-  while (true) {
+  while (1) {
     int D, V;
     cin >> D >> V;
-    
-    if (D == 0 && V == 0) {
-      break;
-    }
-    
+    if (D == 0 and V == 0) break;
     double Vt, Vsm, Vc, Vl;
-    double d, l, h;
-    
     Vt = PI * D * D * D / 4.0;
     Vt -= V;
-    
+    double d, l, h;
     l = 0; h = D;
-    while (true) {
+    while (1) {
       d = (l + h) / 2;
       Vsm = PI * d * d * d / 4.0;
       Vc = PI * (D - d) * (D * D + D * d + d * d) / 24.0;
       Vl = Vsm + 2 * Vc;
-      
-      if (abs(Vt - Vl) < EPS) {
-        break;
-      } else if (Vt - Vl > EPS) {
-        l = d;
-      } else {
-        h = d;
-      }
+      if (abs(Vt - Vl) < EPS) break;
+      else if (Vt - Vl > EPS) l = d;
+      else h = d;
     }
     printf("%.9f\n", d);
   }
