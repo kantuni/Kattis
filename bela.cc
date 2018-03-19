c9ca21aa068f2fc0af5f52cef74a6e9dcfc802a2
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-19 15:44:56 -0400

    Update bela.cc

--- bela.cc
+++ bela.cc
@@ -11,25 +11,18 @@ int main() {
   d['9'] = 14;  nd['9'] = 0;
   d['8'] = 0;   nd['8'] = 0;
   d['7'] = 0;   nd['7'] = 0;
-  
   int n;
   cin >> n;
   char b;
   cin >> b;
-  
   int ans = 0;
   n *= 4;
-  
   while (n--) {
     char num, suit;
     cin >> num >> suit;
-    if (suit == b) {
-      ans += d[num];
-    } else {
-      ans += nd[num];
-    }
+    if (suit == b) ans += d[num];
+    else ans += nd[num];
   }
-  
   cout << ans << "\n";
   return 0;
 }
