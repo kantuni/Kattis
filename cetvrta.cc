cc713200bbdcf94fbc80c352ddb0ceddf32d290b
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-19 15:53:04 -0400

    Update cetvrta.cc

--- cetvrta.cc
+++ cetvrta.cc
@@ -8,14 +8,17 @@ int main() {
     cin >> x >> y;
     coords.push_back(make_pair(x, y));
   }
+  
   // (x1, y1), (x1, y2), (x2, y1), (x2, y2)
   // where x1 ≥ x2, y1 ≥ y2
   sort(coords.begin(), coords.end());
+  
   int dx, dy;
   pair<int, int> ans;
   if (coords[0].first == coords[1].first) {
     dx = coords[2].first - coords[0].first;
     dy = coords[1].second - coords[0].second;
+    
     if (coords[2].second == coords[0].second) {
       ans = make_pair(coords[0].first + dx, coords[0].second + dy);
     } else {
@@ -24,6 +27,7 @@ int main() {
   } else {
     dx = coords[2].first - coords[0].first;
     dy = coords[2].second - coords[1].second;
+    
     if (coords[2].second == coords[0].second) {
       ans = make_pair(coords[2].first - dx, coords[2].second - dy);
     } else {
