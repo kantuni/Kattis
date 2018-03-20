660459d17b29496070e78432cb5d51ecca429f22
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-19 20:25:46 -0400

    Update alphabetspam.cc

--- alphabetspam.cc
+++ alphabetspam.cc
@@ -6,15 +6,10 @@ int main() {
   cin >> s;
   int w = 0, lc = 0, uc = 0, sym = 0;
   for (auto c: s) {
-    if (c == '_') {
-      w++;
-    } else if (c >= 'a' && c <= 'z') {
-      lc++;
-    } else if (c >= 'A' && c <= 'Z') {
-      uc++;
-    } else {
-      sym++;
-    }
+    if (c == '_') w++;
+    else if (c >= 'a' && c <= 'z') lc++;
+    else if (c >= 'A' && c <= 'Z') uc++;
+    else sym++;
   }
   printf("%.7f\n", 1.0 * w / s.size());
   printf("%.7f\n", 1.0 * lc / s.size());
