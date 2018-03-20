5c66cd11f233b50734ba158c1140d7c5d7036d61
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-20 16:03:05 -0400

    Update conundrum.cc

--- conundrum.cc
+++ conundrum.cc
@@ -4,14 +4,12 @@ using namespace std;
 int main() {
   string s;
   cin >> s;
-  
   int ans = 0;
   for (int i = 0; i < s.size() - 2; i += 3) {
     if (s[i] != 'P') ans++;
     if (s[i + 1] != 'E') ans++;
     if (s[i + 2] != 'R') ans++;
   }
-  
   cout << ans << "\n";
   return 0;
 }
