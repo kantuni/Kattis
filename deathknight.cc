3079ee04bddafcb8f21f7054da57b461b86a232c
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-21 19:34:55 -0400

    Update deathknight.cc

--- deathknight.cc
+++ deathknight.cc
@@ -4,12 +4,10 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  
   int ans = 0;
   while (n--) {
     string s;
     cin >> s;
-    
     bool won = true;
     for (int i = 0; i < s.size() - 1; i++) {
       if (s[i] == 'C' && s[i + 1] == 'D') {
@@ -17,12 +15,8 @@ int main() {
         break;
       }
     }
-    
-    if (won) {
-      ans++;
-    }
+    if (won) ans++;
   }
-  
   cout << ans << "\n";
   return 0;
 }
