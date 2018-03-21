98897cb23c80e6e6d8b94b8ed1e5fe421e15e823
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-21 19:34:29 -0400

    Update datum.cc

--- datum.cc
+++ datum.cc
@@ -1,23 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-
 int main() {
   int d, m;
   cin >> d >> m;
-  
-  vi dinm = {
+  vector<int> dinm = {
     31, 28, 31, 
     30, 31, 30, 
     31, 31, 30, 
     31, 30, 31
   };
-  
-  vvi calendar(12);
+  vector<vector<int> > calendar(12);
   int start = 3; // 1.1.2009 = Thursday
-  
   for (int i = 0; i < dinm.size(); i++) {
     calendar[i].resize(dinm[i]);
     calendar[i][0] = start;
@@ -26,12 +20,10 @@ int main() {
     }
     start = (calendar[i][dinm[i] - 1] + 1) % 7;
   }
-  
   vector<string> dofw = {
     "Monday", "Tuesday", "Wednesday", 
     "Thursday", "Friday", "Saturday", "Sunday"
   };
-  
   cout << dofw[calendar[m - 1][d - 1]] << "\n";
   return 0;
 }
