ce86675c89471bf8690894223b03cf7984ff1ae7
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-21 19:36:05 -0400

    Update detaileddifferences.cc

--- detaileddifferences.cc
+++ detaileddifferences.cc
@@ -4,18 +4,14 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  
   for (int i = 0; i < n; i++) {
     string a, b;
     cin >> a >> b;
-    
-    cout << a << "\n";
-    cout << b << "\n";
+    cout << a << "\n" << b << "\n";
     for (int i = 0; i < a.size(); i++) {
       cout << (a[i] == b[i] ? "." : "*");
     }
     cout << "\n\n";
   }
-  
   return 0;
 }
