41e07f5583b0e1e455221226e91d1b612737c88a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-22 16:48:52 -0400

    Update erase.cc

--- erase.cc
+++ erase.cc
@@ -7,21 +7,16 @@ int main() {
   string before, after;
   cin >> before >> after;
   if (n % 2 == 1) {
-    for (char &bit: after) {
-      bit = (bit == '0') ? '1' : '0';
-    }
+    for (char &bit: after) bit = (bit == '0') ? '1' : '0';
   }
   bool failed = false;
   for (int i = 0; i < before.size(); i++) {
     if (before[i] != after[i]) {
       failed = true;
+      break;
     }
   }
-  
-  if (failed) {
-    cout << "Deletion failed\n";
-  } else {
-    cout << "Deletion succeeded\n";
-  }
+  if (failed) cout << "Deletion failed\n";
+  else cout << "Deletion succeeded\n";
   return 0;
 }
