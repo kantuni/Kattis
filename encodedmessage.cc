aa83f443391f3db773273d4166eaa089096b5dd7
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-22 16:46:55 -0400

    Update encodedmessage.cc

--- encodedmessage.cc
+++ encodedmessage.cc
@@ -1,25 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef vector<char> vc;
-typedef vector<vc> vvc;
-
 string decode(string s) {
   int n = sqrt(s.size());
-  vvc square(n, vc(n));
-  
+  vector<vector<char> > square(n, vector<char>(n));
   int next = 0;
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      square[i][j] = s[next++];
-    }
+    for (int j = 0; j < n; j++) square[i][j] = s[next++];
   }
-
   string m = "";
   for (int j = n - 1; j >= 0; j--) {
-    for (int i = 0; i < n; i++) {
-      m += string(1, square[i][j]);
-    }
+    for (int i = 0; i < n; i++) m += string(1, square[i][j]);
   }
   return m;
 }
