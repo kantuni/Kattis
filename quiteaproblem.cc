bf140e3e7518d268d652ba019fb926719ed86e28
kantuni <henrikh.kantuni@gmail.com>
2018-03-22 17:00:01 -0400

    Quite a Problem

---
+++ quiteaproblem.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string line;
+  while (getline(cin, line)) {
+    transform(line.begin(), line.end(), line.begin(), ::tolower);
+    if (line.find("problem") != string::npos) cout << "yes\n";
+    else cout << "no\n";
+  }
+  return 0;
+}
