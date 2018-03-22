8cfe6161b3ae105cd7757c2db45c1c01756d1004
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-22 16:49:26 -0400

    Update estimatingtheareaofacircle.cc

--- estimatingtheareaofacircle.cc
+++ estimatingtheareaofacircle.cc
@@ -4,16 +4,11 @@ using namespace std;
 int main() {
   double r;
   int m, c;
-  
   while (cin >> r >> m >> c) {
-    if (r == 0 && m == 0 && c == 0) {
-      break;
-    }
-    
+    if (r == 0 and m == 0 and c == 0) break;
     double carea = acos(-1.0) * r * r;
     double sarea = 4 * r * r;
     printf("%.7f %.7f\n", carea, sarea * c / m);
   }
-  
   return 0;
 }
