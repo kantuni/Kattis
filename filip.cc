b9c9feaf80af6328741a0abcacdcfc1c72ec8fd2
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-23 08:25:29 -0400

    Update filip.cc

--- filip.cc
+++ filip.cc
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
   int a, b;
   cin >> a >> b;
-  
   int ra = 0, rb = 0;
   for (int i = 0; i < 3; i++) {
     ra += (a % 10) * pow(10, 2 - i);
@@ -12,7 +11,6 @@ int main() {
     rb += (b % 10) * pow(10, 2 - i);
     b /= 10;
   }
-  
   cout << max(ra, rb) << "\n";  
   return 0;
 }
