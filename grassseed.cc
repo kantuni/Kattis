ac9ea770249eed70f8f25244b0f205999d04a848
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-24 06:23:09 -0400

    Update grassseed.cc

--- grassseed.cc
+++ grassseed.cc
@@ -5,14 +5,12 @@ int main() {
   double c;
   int n;
   cin >> c >> n;
-  
   double ans = 0;
   while (n--) {
     double l, w;
     cin >> l >> w;
     ans += l * w;
   }
-  
   printf("%.7f\n", ans * c);
   return 0;
 }
