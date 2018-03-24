7113fce4f6249d695905c26cd66142b3e0774d53
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-24 08:56:30 -0400

    Update icpcawards.cc

--- icpcawards.cc
+++ icpcawards.cc
@@ -4,17 +4,14 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  
   unordered_set<string> memo;
   string u, t;
-  
   while (n--) {
     cin >> u >> t;
-    if (memo.count(u) == 0 && memo.size() < 12) {
+    if (!memo.count(u) and memo.size() < 12) {
       memo.insert(u);
       cout << u << " " << t << "\n";
     }
   }
-  
   return 0;
 }
