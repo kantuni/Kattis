3d024a4e3286f73dea60cfb84c1b9ed891a5aba1
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-25 08:10:27 -0400

    Update herman.cc

--- herman.cc
+++ herman.cc
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define PI 3.14159265359
-
 int main() {
   int r;
   cin >> r;
   cout << fixed << setprecision(6);
-  cout << PI * r * r << "\n";
+  cout << acos(-1.0) * r * r << "\n";
   cout << 2.0 * r * r << "\n";
   return 0;
 }
