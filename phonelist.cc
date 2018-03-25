96fbdcb1e31b2e00fd9abbfc2a1cd7fb56ce7c06
kantuni <henrikh.kantuni@gmail.com>
2018-03-25 07:36:33 -0400

    Phone List

---
+++ phonelist.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<string> numbers(n);
+    for (int i = 0; i < n; i++) cin >> numbers[i];
+    sort(numbers.begin(), numbers.end());
+    bool ok = true;
+    for (int i = 1; i < n; i++) {
+      string prev = numbers[i - 1];
+      string sub = numbers[i].substr(0, prev.size());
+      if (sub == prev) {
+        ok = false;
+        break;
+      }
+    }
+    cout << (ok ? "YES\n" : "NO\n");
+  }
+  return 0;
+}
