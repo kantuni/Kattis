4eae4c903079d4a5c09e1ddf2f41909e8b259461
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-26 12:32:00 -0400

    Update keytocrypto.cc

--- keytocrypto.cc
+++ keytocrypto.cc
@@ -8,13 +8,11 @@ char shift(char c, char k) {
 int main() {
   string c, k;
   cin >> c >> k;
-  
   for (int i = 0; i < c.size(); i++) {
     char l = shift(c[i], k[i]);
     cout << l;
     k += string(1, l);
   }
   cout << "\n";
-  
   return 0;
 }
