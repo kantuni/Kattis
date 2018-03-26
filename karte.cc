e22a6f61374e0f2a06467eeba2f58db8af957413
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-26 12:30:48 -0400

    Update karte.cc

--- karte.cc
+++ karte.cc
@@ -4,10 +4,8 @@ using namespace std;
 int main() {
   string s;
   cin >> s;
-  
   set<string> cards;
   map<char, int> suits;
-  
   for (int i = 0; i < s.size(); i += 3) {
     string c = s.substr(i, 3);
     if (cards.count(c) > 0) {
@@ -17,7 +15,6 @@ int main() {
     cards.insert(c);
     suits[s[i]]++;
   }
-  
   cout << 13 - suits['P'] << " ";
   cout << 13 - suits['K'] << " ";
   cout << 13 - suits['H'] << " ";
