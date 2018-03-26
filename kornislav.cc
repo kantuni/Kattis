aa035e7aa63e3190fa2bc5102d647b7f88c5eb4d
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-26 12:32:28 -0400

    Update kornislav.cc

--- kornislav.cc
+++ kornislav.cc
@@ -3,9 +3,7 @@ using namespace std;
 
 int main() {
   vector<int> steps(4);
-  for (int i = 0; i < 4; i++) {
-    cin >> steps[i];
-  }
+  for (int i = 0; i < 4; i++) cin >> steps[i];
   sort(steps.begin(), steps.end());
   cout << steps[0] * steps[2] << "\n";
   return 0;
