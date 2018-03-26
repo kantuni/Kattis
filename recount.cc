91cc859f1a7e335db83a039b01ae3eac5c142a81
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-26 15:52:00 -0400

    Update recount.cc

--- recount.cc
+++ recount.cc
@@ -3,20 +3,17 @@ using namespace std;
 
 int main() {
   unordered_map<string, int> memo;
-  while (1) {
+  while (true) {
     string vote;
     getline(cin, vote);
     if (vote == "***") break;
     memo[vote]++;
   }
   vector<int> votes;
-  for (auto m: memo) {
-    votes.push_back(m.second);
-  }
+  for (auto m: memo) votes.push_back(m.second);
   sort(votes.rbegin(), votes.rend());
-  if (votes[0] == votes[1]) {
-    cout << "Runoff!\n";
-  } else {
+  if (votes[0] == votes[1]) cout << "Runoff!\n";
+  else {
     for (auto m: memo) {
       if (m.second == votes[0]) {
         cout << m.first << "\n";
