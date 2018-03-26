7ac71087fb531445538f2aabc4ee397bb4c6bb38
kantuni <henrikh.kantuni@gmail.com>
2018-03-26 15:43:24 -0400

    Update busnumbers.cc

--- busnumbers.cc
+++ busnumbers.cc
@@ -1,31 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void print(int start, int end) { 
+  if (start == end) cout << start;
+  else if (end - start == 1) cout << start << " " << end;
+  else cout << start << "-" << end;
+}
+
 int main() {
   int n;
   cin >> n;
   vector<int> numbers(n);
   for (int i = 0; i < n; i++) cin >> numbers[i];
   sort(numbers.begin(), numbers.end());
-  map<int, vector<int> > range;
-  range[numbers[0]].push_back(numbers[0]);
+  int start = numbers[0], end = start;
   for (int i = 1; i < n; i++) {
-    if (numbers[i] - numbers[i - 1] == 1) {
-      int curr = numbers[i];
-      while (!range.count(curr)) curr--;
-      range[curr].push_back(numbers[i]);
-    } else range[numbers[i]].push_back(numbers[i]);
-  }
-  for (auto kv: range) {
-    cout << kv.first;
-    if (kv.second.size() == 1) {
+    if (numbers[i] - numbers[i - 1] == 1) end = numbers[i];
+    else {
+      print(start, end);
       cout << " ";
-      continue;
+      start = numbers[i];
+      end = start;
     }
-    else if (kv.second.size() == 2) cout << " " << kv.second[1];
-    else cout << "-" << kv.second[kv.second.size() - 1];
-    cout << " ";
   }
+  print(start, end);
   cout << "\n";
   return 0;
 }
