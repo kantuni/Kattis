99d4a39894bebb8acee07c65e36dc401b2bf7403
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-27 09:34:00 -0400

    Update ladder.cc

--- ladder.cc
+++ ladder.cc
@@ -1,11 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define PI 3.14159265359
-
 int main() {
   int h, v;
   cin >> h >> v;
-  cout << ceil(h / sin(v * PI / 180)) << "\n";
+  cout << ceil(h / sin(v * acos(-1.0) / 180)) << "\n";
   return 0;
 }
