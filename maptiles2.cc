f391311a1bd011ef65e16324d6944dfcb7d19a3c
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-28 14:03:26 -0400

    Update maptiles2.cc

--- maptiles2.cc
+++ maptiles2.cc
@@ -4,23 +4,18 @@ using namespace std;
 int main() {
   string s;
   cin >> s;
-  
   int zoom = s.size();
   int n = pow(2, zoom);
   int x = 0, y = 0;
-  
   for (int i = 0; i < s.size(); i++) {
-    if (s[i] == '1') {
-      x += n / 2;
-    } else if (s[i] == '2') {
-      y += n / 2;
-    } else if (s[i] == '3') {
+    if (s[i] == '1') x += n / 2;
+    else if (s[i] == '2') y += n / 2;
+    else if (s[i] == '3') {
       x += n / 2;
       y += n / 2;
     }
     n /= 2;
   }
-  
   printf("%d %d %d\n", zoom, x, y);
   return 0;
 }
