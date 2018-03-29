2609d90aa1e2a756d9bfa0b0c19d290bdcadceeb
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-29 06:50:41 -0400

    Update numberfun.cc

--- numberfun.cc
+++ numberfun.cc
@@ -12,11 +12,8 @@ int main() {
     bool c3 = a * b == c;
     bool c4 = 1.0 * a / b == c;
     bool c5 = 1.0 * b / a == c;
-    if (c1 || c2 || c3 || c4 || c5) {
-      cout << "Possible\n";
-    } else {
-      cout << "Impossible\n";
-    }
+    if (c1 || c2 || c3 || c4 || c5) cout << "Possible\n";
+    else cout << "Impossible\n";
   }
   return 0;
 }
