bfc9e63afafeaff52592281d9b9bfd9410a4f15c
kantuni <henrikh.kantuni@gmail.com>
2018-03-29 14:13:24 -0400

    Mjehuric

---
+++ mjehuric.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  vector<int> a(5);
+  for (int i = 0; i < a.size(); i++) cin >> a[i];
+  for (int i = 0; i < a.size(); i++) {
+    for (int j = 0; j < a.size() - 1; j++) {
+      if (a[j] > a[j + 1]) {
+        swap(a[j], a[j + 1]);
+        for (int k = 0; k < a.size(); k++) cout << a[k] << " ";
+        cout << "\n";
+      }
+    }
+  }
+  return 0;
+}
