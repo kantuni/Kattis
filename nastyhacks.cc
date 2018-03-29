e368617a8f96950b613bdbf24ee2adae97e80656
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-29 06:45:38 -0400

    Update nastyhacks.cc

--- nastyhacks.cc
+++ nastyhacks.cc
@@ -4,19 +4,12 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  
   while (n--) {
     int r, e, c;
     cin >> r >> e >> c;
-    
-    if (e - c > r) {
-      cout << "advertise\n";
-    } else if (e - c < r) {
-      cout << "do not advertise\n";
-    } else {
-      cout << "does not matter\n";
-    }
+    if (e - c > r) cout << "advertise\n";
+    else if (e - c < r) cout << "do not advertise\n";
+    else cout << "does not matter\n";
   }
-  
   return 0;
 }
