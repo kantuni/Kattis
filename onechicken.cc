11838a0371ec719dac6b9f90ad217dd3def06ed9
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-30 12:34:25 -0400

    Update onechicken.cc

--- onechicken.cc
+++ onechicken.cc
@@ -4,7 +4,6 @@ using namespace std;
 int main() {
   int n, m;
   cin >> n >> m;
-  
   if (m > n) {
     cout << "Dr. Chaz will have ";
     cout << (m - n == 1 ? "1 piece" : to_string(m - n) + " pieces");
@@ -14,6 +13,5 @@ int main() {
     cout << (n - m == 1 ? "1 more piece" : to_string(n - m) + " more pieces");
     cout << " of chicken!\n";
   }
-  
   return 0;
 }
