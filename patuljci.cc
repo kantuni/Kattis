553e7a8c72379028d6cefbdfaafca2674d068a7b
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-31 13:19:46 -0400

    Update patuljci.cc

--- patuljci.cc
+++ patuljci.cc
@@ -4,12 +4,10 @@ using namespace std;
 int main() {
   vector<int> dwarves(9);
   int sum = 0;
-  
   for (int i = 0; i < 9; i++) {
     cin >> dwarves[i];
     sum += dwarves[i];
   }
-  
   for (int i = 0; i < 8; i++) {
     for (int j = i + 1; j < 9; j++) {
       if (dwarves[i] + dwarves[j] == sum - 100) {
@@ -18,12 +16,8 @@ int main() {
       }
     }
   }
-  
   for (int i = 0; i < 9; i++) {
-    if (dwarves[i] != 0) {
-      cout << dwarves[i] << "\n";
-    }
+    if (dwarves[i] != 0) cout << dwarves[i] << "\n";
   }
-  
   return 0;
 }
