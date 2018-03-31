4d2a06a0782684a658cb78d131efe9edf385c153
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-03-31 13:22:32 -0400

    Update planina.cc

--- planina.cc
+++ planina.cc
@@ -4,12 +4,8 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  
   int ans = 2;
-  while (n--) {
-    ans = 2 * ans - 1;
-  }
-  
+  while (n--) ans = 2 * ans - 1;
   cout << ans * ans << "\n";
   return 0;
 }
