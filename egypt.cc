5e676115f1bbd8405eb5642ac1a90b3049af8a4c
kantuni <henrikh.kantuni@gmail.com>
2018-04-01 06:50:55 -0400

    Egypt

---
+++ egypt.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  while (true) {
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a == 0 and b == 0 and c == 0) break;
+    if (a > b) swap(a, b);
+    if (a > c) swap(a, c);
+    if (b > c) swap(b, c);
+    if (a * a + b * b == c * c) cout << "right\n";
+    else cout << "wrong\n";
+  }
+  return 0;
+}
