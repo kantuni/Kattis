be784a6ac996eded0f8eaeef63f154aaade34e9b
kantuni <henrikh.kantuni@gmail.com>
2018-04-01 09:25:23 -0400

    In Or Out

---
+++ mandelbrot.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t = 1;
+  string line;
+  while (getline(cin, line)) {
+    pair<double, double> z = {0.0, 0.0}, c;
+    int r;
+    istringstream iss(line);
+    iss >> c.first >> c.second >> r;
+    bool in = true;
+    while (r--) {
+      z = make_pair(
+        z.first * z.first - z.second * z.second + c.first,
+        2.0 * z.first * z.second + c.second
+      );
+      if (z.first * z.first + z.second * z.second > 4.0) {
+        in = false;
+        break;
+      }
+    }
+    cout << "Case " << t++ << ": ";
+    cout << (in ? "IN\n" : "OUT\n");
+  }
+  return 0;
+}
