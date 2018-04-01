168af4353734b2b355ac59792a7cd296ce844b43
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-01 11:18:50 -0400

    Update busnumbers.cc

--- busnumbers.cc
+++ busnumbers.cc
@@ -1,25 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print(int start, int end) { 
-  if (start == end) cout << start;
-  else if (end - start == 1) cout << start << " " << end;
-  else cout << start << "-" << end;
+void print(int start, int end) {
+  if (start == end) {
+    cout << start;
+  } else if (end - start == 1) {
+    cout << start << " " << end;
+  } else {
+    cout << start << "-" << end;
+  }
 }
 
 int main() {
   int n;
   cin >> n;
-  vector<int> numbers(n);
-  for (int i = 0; i < n; i++) cin >> numbers[i];
-  sort(numbers.begin(), numbers.end());
-  int start = numbers[0], end = start;
+  vector<int> nums(n);
+  for (int i = 0; i < n; i++) {
+    cin >> nums[i];
+  }
+  sort(nums.begin(), nums.end());
+  int start = nums[0], end = start;
   for (int i = 1; i < n; i++) {
-    if (numbers[i] - numbers[i - 1] == 1) end = numbers[i];
-    else {
+    if (nums[i] - nums[i - 1] == 1) {
+      end = nums[i];
+    } else {
       print(start, end);
       cout << " ";
-      start = numbers[i];
+      start = nums[i];
       end = start;
     }
   }
