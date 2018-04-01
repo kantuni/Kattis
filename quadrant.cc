3500cc8da606a68592e58769dfcae1e6cd97a343
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-01 06:24:08 -0400

    Update quadrant.cc

--- quadrant.cc
+++ quadrant.cc
@@ -1,21 +1,14 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
   int x, y;
   cin >> x >> y;
-  
   int quad;
-  if (x > 0 && y > 0) {
-    quad = 1;
-  } else if (x > 0 && y < 0) {
-    quad = 4;
-  } else if (x < 0 && y > 0) {
-    quad = 2;
-  } else if (x < 0 && y < 0) {
-    quad = 3;
-  }
-  
+  if (x > 0 and y > 0) quad = 1;
+  else if (x > 0 and y < 0) quad = 4;
+  else if (x < 0 and y > 0) quad = 2;
+  else if (x < 0 and y < 0) quad = 3;
   cout << quad << "\n";
   return 0;
 }
