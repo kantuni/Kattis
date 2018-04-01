78595e8f47c8b86b67c2c62147728cce7ba39cff
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-01 07:26:49 -0400

    Update autori.cc

--- autori.cc
+++ autori.cc
@@ -6,7 +6,9 @@ int main() {
   cin >> l;
   string abbr = string(1, l[0]);
   for (int i = 1; i < l.size() - 1; i++) {
-    if (l[i] == '-') abbr += string(1, l[i + 1]);
+    if (l[i] == '-') {
+      abbr += string(1, l[i + 1]);
+    }
   }
   cout << abbr << "\n";
   return 0;
