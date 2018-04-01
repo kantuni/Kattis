da661e1053a43309d22078f6d7bfd21aef1fdef1
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-01 06:24:42 -0400

    Update quadrant.cc

--- quadrant.cc
+++ quadrant.cc
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
   int x, y;
   cin >> x >> y;
-  int quad;
-  if (x > 0 and y > 0) quad = 1;
-  else if (x > 0 and y < 0) quad = 4;
-  else if (x < 0 and y > 0) quad = 2;
-  else if (x < 0 and y < 0) quad = 3;
-  cout << quad << "\n";
+  int q;
+  if (x > 0 and y > 0) q = 1;
+  else if (x > 0 and y < 0) q = 4;
+  else if (x < 0 and y > 0) q = 2;
+  else if (x < 0 and y < 0) q = 3;
+  cout << q << "\n";
   return 0;
 }
