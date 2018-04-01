5a5b91bda7a48bf9ea240aa75cf10ee31e5d7859
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-01 07:21:14 -0400

    Update abc.cc

--- abc.cc
+++ abc.cc
@@ -3,14 +3,20 @@ using namespace std;
 
 int main() {
   vector<int> numbers(3);
-  for (int i = 0; i < 3; i++) cin >> numbers[i];
+  for (int i = 0; i < numbers.size(); i++) {
+    cin >> numbers[i];
+  }
   sort(numbers.begin(), numbers.end());
   string order;
   cin >> order;
   for (char l: order) {
-    if (l == 'A') cout << numbers[0] << " ";
-    else if (l == 'B') cout << numbers[1] << " ";
-    else cout << numbers[2] << " ";
+    if (l == 'A') {
+      cout << numbers[0] << " ";
+    } else if (l == 'B') {
+      cout << numbers[1] << " ";
+    } else {
+      cout << numbers[2] << " ";
+    }
   }
   cout << "\n";
   return 0;
