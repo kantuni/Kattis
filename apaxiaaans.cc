807acc277f098e67e1c41f956aa0856fe049f27e
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-01 07:25:06 -0400

    Update apaxiaaans.cc

--- apaxiaaans.cc
+++ apaxiaaans.cc
@@ -6,7 +6,9 @@ int main() {
   cin >> name;
   string compact = string(1, name[0]);
   for (int i = 1; i < name.size(); i++) {
-    if (name[i] != name[i - 1]) compact += string(1, name[i]);
+    if (name[i] != name[i - 1]) {
+      compact += string(1, name[i]);
+    }
   }
   cout << compact << "\n";
   return 0;
