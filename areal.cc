223c942c61a397989d545e9d62e224b46bdce21a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-01 07:25:59 -0400

    Update areal.cc

--- areal.cc
+++ areal.cc
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
   unsigned long long a;
   cin >> a;
-  printf("%.7f\n", 4 * sqrt(a));
+  cout << fixed << setprecision(7);
+  cout << 4 * sqrt(a) << "\n";
   return 0;
 }
