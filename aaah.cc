4ac8dc08cb32b26ba7d777f844ad09a6b3c6333b
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-02 06:31:55 -0400

    Update aaah.cc

--- aaah.cc
+++ aaah.cc
@@ -4,6 +4,10 @@ using namespace std;
 int main() {
   string john, doc;
   cin >> john >> doc;
-  cout << (john.size() >= doc.size() ? "go" : "no") << "\n";
+  if (john.size() >= doc.size()) {
+    cout << "go\n";
+  } else {
+    cout << "no\n";
+  }
   return 0;
 }
