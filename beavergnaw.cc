a8176acdf260d786c2b823ba289d5d08dae0b8b0
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-02 06:49:04 -0400

    Update beavergnaw.cc

--- beavergnaw.cc
+++ beavergnaw.cc
@@ -1,29 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define PI 3.14159265359
-#define EPS 1e-11
-
 int main() {
+  double EPS = 1e-11;
   while (true) {
     int D, V;
     cin >> D >> V;
-    if (D == 0 and V == 0) break;
-    double Vt, Vsm, Vc, Vl;
-    Vt = PI * D * D * D / 4.0;
+    if (D == 0 and V == 0) {
+      break;
+    }
+    double Vt;
+    Vt = acos(-1.0) * D * D * D / 4.0;
     Vt -= V;
-    double d, l, h;
-    l = 0; h = D;
+    double Vsm, Vc, Vl;
+    double d, l = 0, h = D;
     while (true) {
       d = (l + h) / 2;
-      Vsm = PI * d * d * d / 4.0;
-      Vc = PI * (D - d) * (D * D + D * d + d * d) / 24.0;
+      Vsm = acos(-1.0) * d * d * d / 4.0;
+      Vc = acos(-1.0) * (D - d) * (D * D + D * d + d * d) / 24.0;
       Vl = Vsm + 2 * Vc;
-      if (abs(Vt - Vl) < EPS) break;
-      else if (Vt - Vl > EPS) l = d;
-      else h = d;
+      if (abs(Vt - Vl) < EPS) {
+        break;
+      } else if (Vt - Vl > EPS) {
+        l = d;
+      } else {
+        h = d;
+      }
     }
-    printf("%.9f\n", d);
+    cout << fixed << setprecision(9);
+    cout << d << "\n";
   }
   return 0;
 }
