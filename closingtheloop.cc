a2fdfbc0cf47f45ee7b6a0b69bd9c04bae2b2960
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-03 06:41:07 -0400

    Update closingtheloop.cc

--- closingtheloop.cc
+++ closingtheloop.cc
@@ -12,15 +12,20 @@ int main() {
       int l;
       char c;
       cin >> l >> c;
-      if (c == 'R') red.push_back(l);
-      else blue.push_back(l);
+      if (c == 'R') {
+        red.push_back(l);
+      } else {
+        blue.push_back(l);
+      }
     }
     sort(red.rbegin(), red.rend());
     sort(blue.rbegin(), blue.rend());
     int length = 0;
     for (int j = 0; j < min(red.size(), blue.size()); j++) {
       length += red[j] + blue[j] - 1;
-      if (j != 0) length -= 1;
+      if (j != 0) {
+        length -= 1;
+      }
     }
     printf("Case #%d: %d\n", i, (length - 1 >= 0) ? length - 1 : 0);
   }
