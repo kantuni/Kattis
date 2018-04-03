a7d8131a36bdd766457fd0d35097f670e841bf61
kantuni <henrikh.kantuni@gmail.com>
2018-04-03 09:51:22 -0400

    Permutation Encryption - RTE

---
+++ permutationencryption.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  while (true) {
+    int n;
+    cin >> n;
+    if (n == 0) {
+      break;
+    }
+    vector<int> perm(n);
+    for (int i = 0; i < n; i++) {
+      cin >> perm[i];
+    }
+    cin.ignore();
+    string m;
+    getline(cin, m);
+    while (m.size() % n) {
+      m += " ";
+    }
+    cout << "'";
+    for (int i = 0; i < m.size() - n + 1; i += n) {
+      for (int j = 0; j < n; j++) {
+        cout << m[i + perm[j] - 1];
+      }
+    }
+    cout << "'\n";
+  }
+  return 0;
+}
