3e4b0790fcc571081648fd28552337c45ef4cbaf
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-03 06:42:23 -0400

    Update conundrum.cc

--- conundrum.cc
+++ conundrum.cc
@@ -6,9 +6,15 @@ int main() {
   cin >> s;
   int ans = 0;
   for (int i = 0; i < s.size() - 2; i += 3) {
-    if (s[i] != 'P') ans++;
-    if (s[i + 1] != 'E') ans++;
-    if (s[i + 2] != 'R') ans++;
+    if (s[i] != 'P') {
+      ans++;
+    }
+    if (s[i + 1] != 'E') {
+      ans++;
+    }
+    if (s[i + 2] != 'R') {
+      ans++;
+    }
   }
   cout << ans << "\n";
   return 0;
