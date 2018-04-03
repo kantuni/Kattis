aaf2420d59025248bbef8559de10c2bd35e725c9
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-03 06:38:58 -0400

    Update cetiri.cc

--- cetiri.cc
+++ cetiri.cc
@@ -4,11 +4,21 @@ using namespace std;
 int main() {
   int a, b, c;
   cin >> a >> b >> c;
-  if (a > b) swap(a, b);
-  if (a > c) swap(a, c);
-  if (b > c) swap(b, c);
-  if (c - b == b - a) cout << c + c - b << "\n";
-  else if (c - b > b - a) cout << b + b - a << "\n";
-  else cout << a + c - b << "\n";
+  if (a > b) {
+    swap(a, b);
+  }
+  if (a > c) {
+    swap(a, c);
+  }
+  if (b > c) {
+    swap(b, c);
+  }
+  if (c - b == b - a) {
+    cout << c + c - b << "\n";
+  } else if (c - b > b - a) {
+    cout << b + b - a << "\n";
+  } else {
+    cout << a + c - b << "\n";
+  }
   return 0;
 }
