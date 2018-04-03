4ba8ffaf7affc2fa7c5ba9d939c61c5ed6eeed26
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-03 06:41:55 -0400

    Update compoundwords.cc

--- compoundwords.cc
+++ compoundwords.cc
@@ -4,7 +4,9 @@ using namespace std;
 int main() {
   vector<string> words;
   string w;
-  while (cin >> w) words.push_back(w);
+  while (cin >> w) {
+    words.push_back(w);
+  }
   set<string> s;
   for (int i = 0; i < words.size(); i++) {
     for (int j = i + 1; j < words.size(); j++) {
@@ -12,6 +14,8 @@ int main() {
       s.insert(words[j] + words[i]);
     }
   }
-  for (auto cw: s) cout << cw << "\n";
+  for (auto cw: s) {
+    cout << cw << "\n";
+  }
   return 0;
 }
