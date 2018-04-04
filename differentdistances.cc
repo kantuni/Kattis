b86e435f135f2c4fce026abbb574bb79a119e4d3
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-04 08:00:58 -0400

    Update differentdistances.cc

--- differentdistances.cc
+++ differentdistances.cc
@@ -5,7 +5,9 @@ int main() {
   long double x1, y1, x2, y2, p, ans;
   while (true) {
     cin >> x1;
-    if (x1 == 0) break;
+    if (x1 == 0) {
+      break;
+    }
     cin >> y1 >> x2 >> y2 >> p;
     ans = pow(abs(x1 - x2), p) + pow(abs(y1 - y2), p);
     ans = pow(ans, 1.0 / p);
