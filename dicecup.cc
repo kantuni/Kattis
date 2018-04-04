f5b304841065f0be7e196b0d4457c953fad15b54
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-04 07:59:31 -0400

    Update dicecup.cc

--- dicecup.cc
+++ dicecup.cc
@@ -6,11 +6,15 @@ int main() {
   cin >> n >> m;
   vector<int> res(n + m + 1, 0);
   for (int i = 1; i <= n; i++) {
-    for (int j = 1; j <= m; j++) res[i + j]++;
+    for (int j = 1; j <= m; j++) {
+      res[i + j]++;
+    }
   }
   int hp = *max_element(res.begin(), res.end());
   for (int i = 0; i < res.size(); i++) {
-    if (res[i] == hp) cout << i << "\n";
+    if (res[i] == hp) {
+      cout << i << "\n";
+    }
   }
   return 0;
 }
