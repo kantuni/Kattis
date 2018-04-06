5d93a37c8b58a5206918e4645cd4a91a9f386919
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-05 20:57:44 -0400

    Update estimatingtheareaofacircle.cc

--- estimatingtheareaofacircle.cc
+++ estimatingtheareaofacircle.cc
@@ -5,7 +5,9 @@ int main() {
   double r;
   int m, c;
   while (cin >> r >> m >> c) {
-    if (r == 0 and m == 0 and c == 0) break;
+    if (r == 0 and m == 0 and c == 0) {
+      break;
+    }
     double carea = acos(-1.0) * r * r;
     double sarea = 4 * r * r;
     printf("%.7f %.7f\n", carea, sarea * c / m);
