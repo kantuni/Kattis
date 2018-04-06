ddd577de187dc04ad53e8fc167146a2f35110019
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-05 20:45:06 -0400

    Update egypt.cc

--- egypt.cc
+++ egypt.cc
@@ -5,12 +5,23 @@ int main() {
   while (true) {
     int a, b, c;
     cin >> a >> b >> c;
-    if (a == 0 and b == 0 and c == 0) break;
-    if (a > b) swap(a, b);
-    if (a > c) swap(a, c);
-    if (b > c) swap(b, c);
-    if (a * a + b * b == c * c) cout << "right\n";
-    else cout << "wrong\n";
+    if (a == 0 and b == 0 and c == 0) {
+      break;
+    }
+    if (a > b) {
+      swap(a, b);
+    }
+    if (a > c) {
+      swap(a, c);
+    }
+    if (b > c) {
+      swap(b, c);
+    }
+    if (a * a + b * b == c * c) {
+      cout << "right\n";
+    } else {
+      cout << "wrong\n";
+    }
   }
   return 0;
 }
