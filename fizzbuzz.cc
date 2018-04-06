461cc7a63dfb7a2bf88e46a1b85fd5ab3b458ee9
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-06 18:40:32 -0400

    Update fizzbuzz.cc

--- fizzbuzz.cc
+++ fizzbuzz.cc
@@ -5,10 +5,15 @@ int main() {
   int x, y, n;
   cin >> x >> y >> n;
   for (int i = 1; i <= n; i++) {
-    if (i % x == 0 and i % y == 0) cout << "FizzBuzz\n";
-    else if (i % x == 0) cout << "Fizz\n";  
-    else if (i % y == 0) cout << "Buzz\n";
-    else cout << i << "\n";
+    if (i % x == 0 and i % y == 0) {
+      cout << "FizzBuzz\n";
+    } else if (i % x == 0) {
+      cout << "Fizz\n";
+    } else if (i % y == 0) {
+      cout << "Buzz\n";
+    } else {
+      cout << i << "\n";
+    }
   }
   return 0;
 }
