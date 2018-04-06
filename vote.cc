4c63ce187d46e10c6ac352b23c44de7a646a0b7f
kantuni <henrikh.kantuni@gmail.com>
2018-04-06 00:08:54 -0400

    Popular Vote

---
+++ vote.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<pair<int, int>> votes(n);
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+      int v;
+      cin >> v;
+      votes[i] = make_pair(v, i + 1);
+      sum += v;
+    }
+    sort(votes.rbegin(), votes.rend());
+    pair<int, int> v1 = votes[0], v2 = votes[1];
+    if (v1.first == v2.first) {
+      cout << "no winner\n";
+    } else {
+      cout << ((v1.first > sum / 2.0) ? "majority" : "minority");
+      cout << " winner " << v1.second << "\n";
+    }
+  }
+  return 0;
+}
