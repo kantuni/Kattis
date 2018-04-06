f736179baf927b604f4d17de79d33a0aa42a7346
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-05 20:45:38 -0400

    Update eligibility.cc

--- eligibility.cc
+++ eligibility.cc
@@ -11,9 +11,13 @@ int main() {
     int y1 = stoi(d1.substr(0, 4));
     int y2 = stoi(d2.substr(0, 4));
     cout << name << " ";
-    if (y1 >= 2010 || y2 >= 1991) cout << "eligible\n";
-    else if (courses >= 41) cout << "ineligible\n";
-    else cout << "coach petitions\n";
+    if (y1 >= 2010 || y2 >= 1991) {
+      cout << "eligible\n";
+    } else if (courses >= 41) {
+      cout << "ineligible\n";
+    } else {
+      cout << "coach petitions\n";
+    }
   }
   return 0;
 }
