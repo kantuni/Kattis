58c71c6f0c52fa43b3bc962e24da3e420576035d
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-05 20:50:27 -0400

    Update encodedmessage.cc

--- encodedmessage.cc
+++ encodedmessage.cc
@@ -3,7 +3,7 @@ using namespace std;
 
 string decode(string s) {
   int n = sqrt(s.size());
-  vector<vector<char> > square(n, vector<char>(n));
+  vector< vector<char> > square(n, vector<char>(n));
   int next = 0;
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
