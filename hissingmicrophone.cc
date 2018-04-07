cbb764eb90285f30ccb690a423e352f5a5d06665
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-07 13:33:26 -0400

    Update hissingmicrophone.cc

--- hissingmicrophone.cc
+++ hissingmicrophone.cc
@@ -6,9 +6,15 @@ int main() {
   cin >> s;
   bool hiss = false;
   for (int i = 0; i < s.size() - 1; i++) {
-    if (s[i] == 's' and s[i + 1] == 's') hiss = true;
+    if (s[i] == 's' and s[i + 1] == 's') {
+      hiss = true;
+      break;
+    }
   }
-  if (hiss) cout << "hiss\n";
-  else cout << "no hiss\n";
+  if (hiss) {
+    cout << "hiss\n";
+  } else {
+    cout << "no hiss\n";
+  }
   return 0;
 }
