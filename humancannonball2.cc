c82934c9d955043740931d1507a8cbb8f69950c7
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-07 13:36:09 -0400

    Update humancannonball2.cc

--- humancannonball2.cc
+++ humancannonball2.cc
@@ -1,20 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define EPS 1e-10
-#define PI 3.14159265359
-#define g 9.81
-
 int main() {
+  const double EPS = 1e-10;
+  const double PI = acos(-1.0);
+  const double g = 9.81;
   int n;
   cin >> n;
   while (n--) {
     long double v0, theta, x1, h1, h2, t, y;
     cin >> v0 >> theta >> x1 >> h1 >> h2;
-    t = x1 / (v0 * cos(theta * PI / 180));
-    y = x1 * tan(theta * PI / 180) - 0.5 * g * t * t;
-    if (y - h1 - 1 > EPS and h2 - y - 1 > EPS) cout << "Safe\n";
-    else cout << "Not Safe\n";
+    t = x1 / (v0 * cos(theta * PI / 180.0));
+    y = x1 * tan(theta * PI / 180.0) - 0.5 * g * t * t;
+    if (y - h1 - 1 > EPS and h2 - y - 1 > EPS) {
+      cout << "Safe\n";
+    } else {
+      cout << "Not Safe\n";
+    }
   }
   return 0;
 }
