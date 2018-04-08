4ab2464044d687141c38c456fd5a768beddc4f28
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-08 08:40:31 -0400

    Update judgingmoose.cc

--- judgingmoose.cc
+++ judgingmoose.cc
@@ -4,8 +4,12 @@ using namespace std;
 int main() {
   int l, r;
   cin >> l >> r;
-  if (l == 0 and r == 0) cout << "Not a moose\n";
-  else if (l == r) cout << "Even " << l + r << "\n";
-  else cout << "Odd " << 2 * max(l, r) << "\n";
+  if (l == 0 and r == 0) {
+    cout << "Not a moose\n";
+  } else if (l == r) {
+    cout << "Even " << l + r << "\n";
+  } else {
+    cout << "Odd " << 2 * max(l, r) << "\n";
+  }
   return 0;
 }
