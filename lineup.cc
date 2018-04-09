50621acf02959bd6f1bfc9cd718d34d68f44fc74
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-09 12:38:16 -0400

    Update lineup.cc

--- lineup.cc
+++ lineup.cc
@@ -5,12 +5,18 @@ int main() {
   int n;
   cin >> n;
   vector<string> names(n);
-  for (int i = 0; i < n; i++) cin >> names[i];
+  for (int i = 0; i < n; i++) {
+    cin >> names[i];
+  }
   vector<string> inc(names), dec(names);
   sort(inc.begin(), inc.end());
   sort(dec.rbegin(), dec.rend());
-  if (names == inc) cout << "INCREASING\n";
-  else if (names == dec) cout << "DECREASING\n";
-  else cout << "NEITHER\n";
+  if (names == inc) {
+    cout << "INCREASING\n";
+  } else if (names == dec) {
+    cout << "DECREASING\n";
+  } else {
+    cout << "NEITHER\n";
+  }
   return 0;
 }
