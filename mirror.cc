61597674e2a69e5d0c05e874fe57b0156932fa77
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-09 12:42:45 -0400

    Update mirror.cc

--- mirror.cc
+++ mirror.cc
@@ -4,16 +4,20 @@ using namespace std;
 int main() {
   int T;
   cin >> T;
-  for (int t = 0; t < T; t++) {
+  for (int t = 1; t <= T; t++) {
     int r, c;
     cin >> r >> c;
-    vector<vector<char> > M(r, vector<char>(c));
+    vector<vector<char>> M(r, vector<char>(c));
     for (int i = 0; i < r; i++) {
-      for (int j = 0; j < c; j++) cin >> M[i][j];
+      for (int j = 0; j < c; j++) {
+        cin >> M[i][j];
+      }
     }
-    cout << "Test " << t + 1 << "\n";
+    cout << "Test " << t << "\n";
     for (int i = 0; i < r; i++) {
-      for (int j = 0; j < c; j++) cout << M[r - 1 - i][c - 1 - j];
+      for (int j = 0; j < c; j++) {
+        cout << M[r - 1 - i][c - 1 - j];
+      }
       cout << "\n";
     }
   }
