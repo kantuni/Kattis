28d0052c6e4f0a9abf6b5231ec7f30be85845bf9
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-09 12:37:38 -0400

    Update leftbeehind.cc

--- leftbeehind.cc
+++ leftbeehind.cc
@@ -5,11 +5,18 @@ int main() {
   while (true) {
     int x, y;
     cin >> x >> y;
-    if (x == 0 and y == 0) break;
-    if (x + y == 13) cout << "Never speak again.\n";
-    else if (y > x) cout << "Left beehind.\n";
-    else if (x > y) cout << "To the convention.\n";
-    else cout << "Undecided.\n";
+    if (x == 0 and y == 0) {
+      break;
+    }
+    if (x + y == 13) {
+      cout << "Never speak again.\n";
+    } else if (y > x) {
+      cout << "Left beehind.\n";
+    } else if (x > y) {
+      cout << "To the convention.\n";
+    } else {
+      cout << "Undecided.\n";
+    }
   }
   return 0;
 }
