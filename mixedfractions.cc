bacfe46ddae0f910aae0a01a528cb5c5647a5e70
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-09 12:43:38 -0400

    Update mixedfractions.cc

--- mixedfractions.cc
+++ mixedfractions.cc
@@ -5,7 +5,9 @@ int main() {
   while (true) {
     int num, denom;
     cin >> num >> denom;
-    if (num == 0 and denom == 0) break;
+    if (num == 0 and denom == 0) {
+      break;
+    }
     cout << num / denom << " ";
     cout << num % denom << " / " << denom << "\n";
   }
