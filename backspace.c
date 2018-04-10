c4a2c409bec767f1a3552230e6356fe27e8dde15
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-10 02:18:17 -0400

    Update backspace.c

--- backspace.c
+++ backspace.c
@@ -4,19 +4,18 @@
 int main() {
   size_t size = 1000005;
   char s[size];
-  if (scanf("%s", s) == 1) {
-    size_t len = strlen(s) + 1, curr = 0;
-    char ans[len];
-    for (size_t i = 0; i < len; i++) {
-      if (s[i] == '<') {
-        curr--;
-        ans[curr] = '\0';
-      } else {
-        ans[curr] = s[i];
-        curr++;
-      }
+  int nop = scanf("%s", s);
+  size_t len = strlen(s) + 1, curr = 0;
+  char ans[len];
+  for (size_t i = 0; i < len; i++) {
+    if (s[i] == '<') {
+      curr--;
+      ans[curr] = '\0';
+    } else {
+      ans[curr] = s[i];
+      curr++;
     }
-    printf("%s\n", ans);
   }
+  printf("%s\n", ans);
   return 0;
 }
