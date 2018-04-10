b44461fe30d6a9ab001301104b4b4ec2f915211a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-10 02:11:46 -0400

    Update backspace.c

--- backspace.c
+++ backspace.c
@@ -5,9 +5,8 @@ int main() {
   size_t size = 1000005;
   char s[size];
   if (scanf("%s", s) == 1) {
-    size_t len = strlen(s) + 1;
+    size_t len = strlen(s) + 1, curr = 0;
     char ans[len];
-    size_t curr = 0;
     for (size_t i = 0; i < len; i++) {
       if (s[i] == '<') {
         curr--;
