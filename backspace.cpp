dded142bf4576d7884118d83e2cf6539847172ab
kantuni <henrikh.kantuni@gmail.com>
2018-04-10 00:03:08 -0400

    Backspace

---
+++ backspace.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  string ans;
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] != '<') {
+      ans.push_back(s[i]);
+    } else {
+      ans.pop_back();
+    }
+  }
+  cout << ans << "\n";
+  return 0;
+}
