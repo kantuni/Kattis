bff85048b0db786736d8bbfa669c33e0b89c4594
kantuni <henrikh.kantuni@gmail.com>
2018-04-11 17:40:40 -0400

    Akcija

---
+++ akcija.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> c(n);
+  for (int i = 0; i < n; i++) {
+    cin >> c[i];
+  }
+  sort(c.rbegin(), c.rend());
+  long long ans = 0;
+  for (int i = 0; i < n; i += 3) {
+    ans += c[i];
+    if (i + 1 < n) {
+      ans += c[i + 1];
+    }
+  }
+  cout << ans << "\n";
+  return 0;
+}
