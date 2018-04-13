0d2dc537f403f16a608a246c1b9e2b6fb75c666f
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-12 20:36:04 -0400

    Update ostgotska.cpp

--- ostgotska.cpp
+++ ostgotska.cpp
@@ -5,11 +5,16 @@ int main() {
   int wcnt = 0, aecnt = 0;
   string w;
   while (cin >> w) {
-    if (w.find("ae") != string::npos) aecnt++;
+    if (w.find("ae") != string::npos) {
+      aecnt++;
+    }
     wcnt++;
   }
   double p = 1.0 * aecnt / wcnt;
-  if (p >= 0.4) cout << "dae ae ju traeligt va\n";
-  else cout << "haer talar vi rikssvenska\n";
+  if (p >= 0.4) {
+    cout << "dae ae ju traeligt va\n";
+  } else {
+    cout << "haer talar vi rikssvenska\n";
+  }
   return 0;
 }
