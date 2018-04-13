6f2f668f17d34c0639dcd727cb8a8de1b3e00e45
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-12 20:32:54 -0400

    Update oddgnome.cpp

--- oddgnome.cpp
+++ oddgnome.cpp
@@ -8,7 +8,9 @@ int main() {
     int g;
     cin >> g;
     vector<int> gnomes(g);
-    for (int i = 0; i < g; i++) cin >> gnomes[i];
+    for (int i = 0; i < g; i++) {
+      cin >> gnomes[i];
+    }
     for (int i = 1; i < g; i++) {
       if (gnomes[i] != gnomes[i - 1] + 1) {
         cout << i + 1 << "\n";
