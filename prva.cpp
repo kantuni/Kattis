1011dc089632f992c65b6600442f7e617efd63ac
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-14 00:06:53 -0400

    Update prva.cpp

--- prva.cpp
+++ prva.cpp
@@ -4,31 +4,37 @@ using namespace std;
 int main() {
   int r, c;
   cin >> r >> c;
-  vector<vector<char> > crossword(r, vector<char>(c));
+  vector<vector<char>> CW(r, vector<char>(c));
   for (int i = 0; i < r; i++) {
-    for (int j = 0; j < c; j++) cin >> crossword[i][j];
-  }
-  vector<string> words;
-  // horizontal
-  for (int i = 0; i < r; i++) {
-    string line;
-    for (int j = 0; j < c; j++) line += string(1, crossword[i][j]);
-    // split line by '#'
-    stringstream ss(line);
-    string token;
-    while (getline(ss, token, '#')) {
-      if (token.size() > 1) words.push_back(token);
+    for (int j = 0; j < c; j++) {
+      cin >> CW[i][j];
     }
   }
-  // vertical
-  for (int i = 0; i < c; i++) {
+  vector<string> words;
+  for (int i = 0; i < r; i++) {
     string line;
-    for (int j = 0; j < r; j++) line += string(1, crossword[j][i]);
-    // split line by '#'
+    for (int j = 0; j < c; j++) {
+      line += string(1, CW[i][j]);
+    }
     stringstream ss(line);
     string token;
     while (getline(ss, token, '#')) {
-      if (token.size() > 1) words.push_back(token);
+      if (token.size() > 1) {
+        words.push_back(token);
+      }
+    }
+  }
+  for (int i = 0; i < c; i++) {
+    string line;
+    for (int j = 0; j < r; j++) {
+      line += string(1, CW[j][i]);
+    }
+    stringstream ss(line);
+    string token;
+    while (getline(ss, token, '#')) {
+      if (token.size() > 1) {
+        words.push_back(token);
+      }
     }
   }
   sort(words.begin(), words.end());
