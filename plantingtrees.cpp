65852c84d673a934493e80d08049c2efb606e089
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-14 00:03:39 -0400

    Update plantingtrees.cpp

--- plantingtrees.cpp
+++ plantingtrees.cpp
@@ -5,13 +5,17 @@ int main() {
   int n;
   cin >> n;
   vector<int> t(n);
-  for (int i = 0; i < n; i++) cin >> t[i];
+  for (int i = 0; i < n; i++) {
+    cin >> t[i];
+  }
   sort(t.rbegin(), t.rend());
   int left = t[0], ans = 1;
   for (int i = 1; i < n; i++) {
     left--;
     ans++;
-    if (t[i] > left) left = t[i];
+    if (t[i] > left) {
+      left = t[i];
+    }
   }
   ans += left;
   cout << ans + 1 << "\n";
