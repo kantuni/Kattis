e7249c3eddc322abc122decd2c133922181b04f7
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-14 00:07:54 -0400

    Update ptice.cpp

--- ptice.cpp
+++ ptice.cpp
@@ -11,14 +11,26 @@ int main() {
   string g = "CCAABB";
   int ac = 0, bc = 0, gc = 0;
   for (int i = 0; i < n; i++) {
-    if (ans[i] == a[i % 3]) ac++;
-    if (ans[i] == b[i % 4]) bc++;
-    if (ans[i] == g[i % 6]) gc++;
+    if (ans[i] == a[i % 3]) {
+      ac++;
+    }
+    if (ans[i] == b[i % 4]) {
+      bc++;
+    }
+    if (ans[i] == g[i % 6]) {
+      gc++;
+    }
   }
   int m = max({ac, bc, gc});
   cout << m << "\n";
-  if (ac == m) cout << "Adrian\n";
-  if (bc == m) cout << "Bruno\n";
-  if (gc == m) cout << "Goran\n";
+  if (ac == m) {
+    cout << "Adrian\n";
+  }
+  if (bc == m) {
+    cout << "Bruno\n";
+  }
+  if (gc == m) {
+    cout << "Goran\n";
+  }
   return 0;
 }
