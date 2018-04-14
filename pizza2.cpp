46b62a7a3d02c440c10122ce8496400f32821a9f
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-14 00:02:36 -0400

    Update pizza2.cpp

--- pizza2.cpp
+++ pizza2.cpp
@@ -5,6 +5,7 @@ int main() {
   int r, c;
   cin >> r >> c;
   double ans = 100.0 * (r - c) * (r - c) / (r * r);
-  printf("%.6f\n", ans);
+  cout << fixed << setprecision(6);
+  cout << ans << "\n";
   return 0;
 }
