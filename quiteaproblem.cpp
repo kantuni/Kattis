53ff5d88f4dc22015920d22a56b7a8812327dace
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-15 12:33:26 -0400

    Update quiteaproblem.cpp

--- quiteaproblem.cpp
+++ quiteaproblem.cpp
@@ -5,8 +5,11 @@ int main() {
   string line;
   while (getline(cin, line)) {
     transform(line.begin(), line.end(), line.begin(), ::tolower);
-    if (line.find("problem") != string::npos) cout << "yes\n";
-    else cout << "no\n";
+    if (line.find("problem") != string::npos) {
+      cout << "yes\n";
+    } else {
+      cout << "no\n";
+    }
   }
   return 0;
 }
