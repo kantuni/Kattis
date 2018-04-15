adcb815a2307ea16af513c5299d41dce4dd35af6
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-15 12:31:43 -0400

    Update quadrant.cpp

--- quadrant.cpp
+++ quadrant.cpp
@@ -5,10 +5,15 @@ int main() {
   int x, y;
   cin >> x >> y;
   int q;
-  if (x > 0 and y > 0) q = 1;
-  else if (x > 0 and y < 0) q = 4;
-  else if (x < 0 and y > 0) q = 2;
-  else if (x < 0 and y < 0) q = 3;
+  if (x > 0 and y > 0) {
+    q = 1;
+  } else if (x > 0 and y < 0) {
+    q = 4;
+  } else if (x < 0 and y > 0) {
+    q = 2;
+  } else if (x < 0 and y < 0) {
+    q = 3;
+  }
   cout << q << "\n";
   return 0;
 }
