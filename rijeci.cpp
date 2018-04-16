5a25af103dd887acec401bb73921aac99b98ccbf
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-16 10:20:32 -0400

    Update rijeci.cpp

--- rijeci.cpp
+++ rijeci.cpp
@@ -4,15 +4,13 @@ using namespace std;
 int main() {
   int k;
   cin >> k;
-  
   int a = 0, b = 1;
-  while (k - 1 > 0) {
+  while (k > 1) {
     int tmp = b;
     b = a + b;
     a = tmp;
     k--;
   }
-  
   cout << a << " " << b << "\n";
   return 0;
 }
