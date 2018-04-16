8a3e765fa71d331668eec15961ba396ee70ac4ae
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-16 10:18:21 -0400

    Update reverserot.cpp

--- reverserot.cpp
+++ reverserot.cpp
@@ -4,12 +4,10 @@ using namespace std;
 string encrypt(string m, int n) {
   string sigma = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_.";
   reverse(m.begin(), m.end());
-  
   for (char &l: m) {
     int index = sigma.find(l);
     l = sigma[(index + n) % sigma.size()];
   }
-  
   return m;
 }
 
@@ -17,15 +15,12 @@ int main() {
   while (true) {
     int n;
     cin >> n;
-    
     if (n == 0) {
       break;
     }
-    
     string m;
     cin >> m;
     cout << encrypt(m, n) << "\n";
   }
-  
   return 0;
 }
