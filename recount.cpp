278aff10eb58a95f4df64a352297b41947850fef
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-16 10:14:29 -0400

    Update recount.cpp

--- recount.cpp
+++ recount.cpp
@@ -6,14 +6,19 @@ int main() {
   while (true) {
     string vote;
     getline(cin, vote);
-    if (vote == "***") break;
+    if (vote == "***") {
+      break;
+    }
     memo[vote]++;
   }
   vector<int> votes;
-  for (auto m: memo) votes.push_back(m.second);
+  for (auto m: memo) {
+    votes.push_back(m.second);
+  }
   sort(votes.rbegin(), votes.rend());
-  if (votes[0] == votes[1]) cout << "Runoff!\n";
-  else {
+  if (votes[0] == votes[1]) {
+    cout << "Runoff!" << "\n";
+  } else {
     for (auto m: memo) {
       if (m.second == votes[0]) {
         cout << m.first << "\n";
