71d4a71161e8ff7dea76bcf1f48956a0ab22be95
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-16 10:13:12 -0400

    Update rationalsequence2.cpp

--- rationalsequence2.cpp
+++ rationalsequence2.cpp
@@ -2,15 +2,13 @@
 using namespace std;
 
 int main() {
-  int p;
-  cin >> p;
-  
-  while (p--) {
-    int k, p, q; char c;
+  int P;
+  cin >> P;
+  while (P--) {
+    int k, p, q;
+    char c;
     cin >> k >> p >> c >> q;
-    
     vector<char> path;
-    
     while (p != 1 || q != 1) {
       if (p > q) {
         path.push_back('r');
@@ -20,10 +18,8 @@ int main() {
         q -= p;
       }
     }
-    
     reverse(path.begin(), path.end());
     int l = 0, h = pow(2, path.size());
-    
     for (int i = 0; i < path.size(); i++) {
       if (path[i] == 'r') {
         l += (h - l) / 2;
@@ -31,10 +27,8 @@ int main() {
         h -= (h - l) / 2;
       }
     }
-    
-    int answer = pow(2, path.size()) + l;
-    cout << k << " " << answer << "\n";
+    int ans = pow(2, path.size()) + l;
+    cout << k << " " << ans << "\n";
   }
-  
   return 0;
 }
