65c90bc7778dace80042c1184bba585c355fb239
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-16 10:15:28 -0400

    Update reversebinary.cpp

--- reversebinary.cpp
+++ reversebinary.cpp
@@ -4,18 +4,15 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  
   string bin;
-  while (n != 0) {
+  while (n) {
     bin += to_string(n % 2);
     n /= 2;
   }
-  
   int ans = 0;
   for (int i = 0; i < bin.size(); i++) {
     ans += (bin[bin.size() - 1 - i] - '0') * pow(2, i);
   }
-  
   cout << ans << "\n";
   return 0;
 }
