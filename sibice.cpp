8905be207889ffd49d9730225b85feab8bb09cad
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-17 09:26:51 -0400

    Update sibice.cpp

--- sibice.cpp
+++ sibice.cpp
@@ -4,16 +4,14 @@ using namespace std;
 int main() {
   int n, w, h;
   cin >> n >> w >> h;
-  
   while (n--) {
     int l;
     cin >> l;
     if (l * l <= w * w + h * h) {
-      cout << "DA\n";
+      cout << "DA" << "\n";
     } else {
-      cout << "NE\n";
+      cout << "NE" << "\n";
     }
   }
-  
   return 0;
 }
