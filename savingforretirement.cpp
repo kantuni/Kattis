5b853fd0f9e3e83dedf1e988a4671546e0b25527
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-17 09:21:08 -0400

    Update savingforretirement.cpp

--- savingforretirement.cpp
+++ savingforretirement.cpp
@@ -5,8 +5,7 @@ int main() {
   int b, br, bs, a, as;
   cin >> b >> br >> bs >> a >> as;
   double x = ceil(1.0 * (br - b) * bs / as + a);
-  bool ok = (x - a) * as > (br - b) * bs;
-  if (!ok) {
+  if ((x - a) * as <= (br - b) * bs) {
     x++;
   }
   cout << x << "\n";
