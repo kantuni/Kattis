6498b36578220834a8463621a2971d4605b0e2d2
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-17 09:31:09 -0400

    Update skener.cpp

--- skener.cpp
+++ skener.cpp
@@ -1,20 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef vector<char> vc;
-typedef vector<vc> vvc;
-
 int main() {
   int r, c, zr, zc;
   cin >> r >> c >> zr >> zc;
-  
-  vvc A(r, vc(c)), M(r * zr, vc(c * zc));
+  vector<vector<char>> A(r, vector<char>(c));
+  vector<vector<char>> M(r * zr, vector<char>(c * zc));
   for (int i = 0; i < r; i++) {
     for (int j = 0; j < c; j++) {
       cin >> A[i][j];
     }
   }
-  
   for (int i = 0; i < r * zr; i++) {
     for (int j = 0; j < c * zc; j++) {
       M[i][j] = A[i / zr][j / zc];
@@ -22,6 +18,5 @@ int main() {
     }
     cout << "\n";
   }
-  
   return 0;
 }
