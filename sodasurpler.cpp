b9d7a3be93cab0538afba8ed7e47b0a39263c08c
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-17 09:47:44 -0400

    Update sodasurpler.cpp

--- sodasurpler.cpp
+++ sodasurpler.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
   int e, f, c;
   cin >> e >> f >> c;
-  int total = e + f, count = 0;
+  int total = e + f, ans = 0;
   while (total >= c) {
     total = total - c + 1;
-    count++;
+    ans++;
   }
-  cout << count << "\n";
+  cout << ans << "\n";
   return 0;
 }
