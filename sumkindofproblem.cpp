1a37e242916b4de5e1d057478dcad087db9938b9
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-17 09:52:26 -0400

    Update sumkindofproblem.cpp

--- sumkindofproblem.cpp
+++ sumkindofproblem.cpp
@@ -10,10 +10,14 @@ int main() {
     long long s1 = 0, s2 = 0, s3 = 0;
     s1 = (n + 1) * n / 2;
     for (int i = 1, c = 0; c < 2 * n; i++, c++) {
-      if (i % 2 == 1) s2 += i;
-      else s3 += i;
+      if (i % 2 == 1) {
+        s2 += i;
+      } else {
+        s3 += i;
+      }
     }
-    cout << k << " " << s1 << " " << s2 << " " << s3 << "\n";
+    cout << k << " " << s1 << " ";
+    cout << s2 << " " << s3 << "\n";
   }
   return 0;
 }
