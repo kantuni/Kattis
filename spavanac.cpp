a39155e0c624b1d95ed1e30d743923c704137b83
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-17 09:48:18 -0400

    Update spavanac.cpp

--- spavanac.cpp
+++ spavanac.cpp
@@ -4,12 +4,11 @@ using namespace std;
 int main() {
   int h, m;
   cin >> h >> m;
-  
   if (m > 45) {
     cout << h << " " << m - 45 << "\n";
   } else {
-    cout << (h - 1 + 24) % 24 << " " << (m - 45 + 60) % 60 << "\n";
+    cout << (h - 1 + 24) % 24 << " ";
+    cout << (m - 45 + 60) % 60 << "\n";
   }
-  
   return 0;
 }
