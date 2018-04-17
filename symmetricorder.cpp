77bd344f2540b612e3117de27547d1c1cbebb9e7
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-17 09:53:59 -0400

    Update symmetricorder.cpp

--- symmetricorder.cpp
+++ symmetricorder.cpp
@@ -6,25 +6,20 @@ int main() {
   while (true) {
     int n;
     cin >> n;
-    
     if (n == 0) {
       break;
     }
-    
-    printf("SET %d\n", sn);
+    cout << "SET " << sn << "\n";
     stack<string> s;
-    
     for (int i = 0; i < n; i++) {
       string name;
       cin >> name;
-      
-      if (i % 2 == 1) {
+      if (i % 2) {
         s.push(name);
       } else {
         cout << name << "\n";
       }
     }
-    
     while (!s.empty()) {
       cout << s.top() << "\n";
       s.pop();
