20b23a65473c560f926e36ed49acff7259e9f000
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-18 05:40:29 -0400

    Update zamka.cpp

--- zamka.cpp
+++ zamka.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int dsum(int n) {
   int sum = 0;
-  while (n != 0) {
+  while (n) {
     sum += n % 10;
     n /= 10;
   }
@@ -13,20 +13,17 @@ int dsum(int n) {
 int main() {
   int l, d, x;
   cin >> l >> d >> x;
-  
   for (int n = l; n <= d; n++) {
     if (dsum(n) == x) {
       cout << n << "\n";
       break;
     }
   }
-  
   for (int m = d; m >= l; m--) {
     if (dsum(m) == x) {
       cout << m << "\n";
       break;
     }
   }
-  
   return 0;
 }
