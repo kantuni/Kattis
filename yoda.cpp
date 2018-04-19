a117307115658cade555cd880c94f0d81af513b3
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-19 15:38:11 -0400

    Update yoda.cpp

--- yoda.cpp
+++ yoda.cpp
@@ -5,15 +5,21 @@ int main() {
   string a, b;
   cin >> a >> b;
   if (a.size() < b.size()) {
-    while (a.size() != b.size()) a = "0" + a;
+    while (a.size() != b.size()) {
+      a = "0" + a;
+    }
   } else if (a.size() > b.size()) {
-    while (a.size() != b.size()) b = "0" + b;
+    while (a.size() != b.size()) {
+      b = "0" + b;
+    }
   }
   string x, y;
   for (int i = 0; i < a.size(); i++) {
-    if (a[i] > b[i]) x += string(1, a[i]);
-    else if (a[i] < b[i]) y += string(1, b[i]);
-    else {
+    if (a[i] > b[i]) {
+      x += string(1, a[i]);
+    } else if (a[i] < b[i]) {
+      y += string(1, b[i]);
+    } else {
       x += string(1, a[i]);
       y += string(1, b[i]);
     }
