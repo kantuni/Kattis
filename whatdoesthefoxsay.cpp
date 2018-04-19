bec8e423b6c3760afdcf73548575ba7e551447dc
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-19 15:43:10 -0400

    Update whatdoesthefoxsay.cpp

--- whatdoesthefoxsay.cpp
+++ whatdoesthefoxsay.cpp
@@ -11,19 +11,25 @@ int main() {
     istringstream iss(line);
     vector<string> sounds;
     string sound;
-    while (iss >> sound) sounds.push_back(sound);
+    while (iss >> sound) {
+      sounds.push_back(sound);
+    }
     unordered_set<string> known;
     while (true) {
       string line;
       getline(cin, line);
-      if (line == "what does the fox say?") break;
+      if (line == "what does the fox say?") {
+        break;
+      }
       string animal, goes, sound;
       istringstream iss(line);
       iss >> animal >> goes >> sound;
       known.insert(sound);
     }
-    for (auto sound: sounds) {
-      if (!known.count(sound)) cout << sound << " ";
+    for (auto s: sounds) {
+      if (!known.count(s)) {
+        cout << s << " ";
+      }
     }
     cout << "\n";
   }
