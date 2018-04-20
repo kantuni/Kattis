7f887081f2e965cd2d05679e9d33eb1e70691cbb
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-20 03:45:51 -0700

    Update volim.cpp

--- volim.cpp
+++ volim.cpp
@@ -2,20 +2,18 @@
 using namespace std;
 
 int main() {
-  int k, n, passed = 0;
+  int k, n;
   cin >> k >> n;
-  
+  int passed = 0;
   while (n--) {
-    int t; char z;
+    int t;
+    char z;
     cin >> t >> z;
-    
     passed += t;
-    
-    if (z == 'T' && passed < 210) {
+    if (z == 'T' and passed < 210) {
       k = k % 8 + 1;
     }
   }
-  
   cout << k << "\n";
   return 0;
 }
