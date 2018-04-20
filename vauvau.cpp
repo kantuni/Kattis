57aafe97c5e6408f96425aeefd0d258de4f132fa
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-20 03:47:20 -0700

    Update vauvau.cpp

--- vauvau.cpp
+++ vauvau.cpp
@@ -6,34 +6,34 @@ int main() {
   cin >> a >> b >> c >> d;
   vector<int> heroes(3);
   cin >> heroes[0] >> heroes[1] >> heroes[2];
-  
   for (int i = 0; i < 3; i++) {
     int l1 = 0, r1 = a;
     bool a1 = true;
     while (true) {
-      if (l1 <= heroes[i] and heroes[i] <= r1) break;
+      if (l1 <= heroes[i] and heroes[i] <= r1) {
+        break;
+      }
       l1 = r1;
       r1 += a1 ? b : a;
       a1 = !a1;
     }
-    
     int l2 = 0, r2 = c;
     bool a2 = true;
     while (true) {
-      if (l2 <= heroes[i] and heroes[i] <= r2) break;
+      if (l2 <= heroes[i] and heroes[i] <= r2) {
+        break;
+      }
       l2 = r2;
       r2 += a2 ? d : c;
       a2 = !a2;
     }
-    
     if (a1 and a2) {
-      cout << "both\n";
+      cout << "both" << "\n";
     } else if (a1 or a2) {
-      cout << "one\n";
+      cout << "one" << "\n";
     } else {
-      cout << "none\n";
+      cout << "none" << "\n";
     }
   }
-  
   return 0;
 }
