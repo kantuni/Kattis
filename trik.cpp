dd737f42252f1c2d66f3c6dcc3d475dbecd32ed3
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-21 13:58:52 -0700

    Update trik.cpp

--- trik.cpp
+++ trik.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main() { 
   string moves;
   cin >> moves;
-  
   int cups[3] = {1, 0, 0};
   for (int i = 0; i < moves.size(); i++) {
     if (moves[i] == 'A') {
@@ -15,12 +14,10 @@ int main() {
       swap(cups[0], cups[2]);
     }
   }
-  
   for (int i = 0; i < 3; i++) {
     if (cups[i] == 1) {
       cout << i + 1 << "\n";
     }
   }
-  
   return 0;
 }
