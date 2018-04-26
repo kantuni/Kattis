dab8776451642ec7e7f8bea017b4ae0fd78d9262
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-26 11:42:00 -0400

    Update dicecup.cpp

--- dicecup.cpp
+++ dicecup.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main() {
   int n, m;
   cin >> n >> m;
+  int hp = -1;
   vector<int> res(n + m + 1, 0);
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= m; j++) {
       res[i + j]++;
+      hp = max(hp, res[i + j]);
     }
   }
-  int hp = *max_element(res.begin(), res.end());
   for (int i = 0; i < res.size(); i++) {
     if (res[i] == hp) {
       cout << i << "\n";
