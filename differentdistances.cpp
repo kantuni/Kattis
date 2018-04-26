fe61e6c7813d8b18d63ce0b661f602fba0d7c900
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-26 11:46:42 -0400

    Update differentdistances.cpp

--- differentdistances.cpp
+++ differentdistances.cpp
@@ -11,7 +11,8 @@ int main() {
     cin >> y1 >> x2 >> y2 >> p;
     ans = pow(abs(x1 - x2), p) + pow(abs(y1 - y2), p);
     ans = pow(ans, 1.0 / p);
-    printf("%.10Lf\n", ans);
+    cout << fixed << setprecision(10);
+    cout << ans << "\n";
   }
   return 0;
 }
