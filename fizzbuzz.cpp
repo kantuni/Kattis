9e8d6a2de921264b9dc4cf41015b21e7ebf8a85a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-04-28 12:51:37 -0400

    Update fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
   int x, y, n;
   cin >> x >> y >> n;
-  for (int i = 1; i <= n; i++) {
+  for (int i = 1; i < n + 1; i++) {
     if (i % x == 0 and i % y == 0) {
-      cout << "FizzBuzz\n";
+      cout << "FizzBuzz" << "\n";
     } else if (i % x == 0) {
-      cout << "Fizz\n";
+      cout << "Fizz" << "\n";
     } else if (i % y == 0) {
-      cout << "Buzz\n";
+      cout << "Buzz" << "\n";
     } else {
       cout << i << "\n";
     }
