21ed222252970c1da23909a4d9b10a40ca5752c5
kantuni <henrikh.kantuni@gmail.com>
2018-04-30 21:25:11 -0400

    Update collatz.cpp

--- collatz.cpp
+++ collatz.cpp
@@ -1,44 +1,70 @@
+// NOTE: not solved yet
+
 #include <bits/stdc++.h>
 using namespace std;
+
+// globals
+unordered_map<int, int> ntoc;
+unordered_map<int, vector<int>> cton;
+vector<int> at;
+
+void meet(vector<int> array) {
+  for (auto val: array) {
+    cout << val << " ";
+  }
+  cout << "\n";
+  int size = array.size();
+  if (array.size() > 1 and array[size - 1] == array[size - 2]) {
+    at.push_back(array[size - 1]);
+  }
+}
+
+int populate(int n, int m) {
+  if (ntoc.count(n)) {
+    int ans = ntoc[n] + m;
+    cton[ans].push_back(n);
+    meet(cton[ans]);
+    return ans;
+  }
+  int next;
+  if (n % 2) {
+    next = 3 * n + 1;
+  } else {
+    next = n / 2;
+  }
+  int ans = populate(next, m + 1);
+  ntoc[next] = ans - m - 1;
+  cton[ans - m - 1].push_back(next);
+  return ans;
+}
 
 int main() {
   int a, b;
   while (cin >> a >> b) {
+    ntoc[1] = 0;
+    cton[0] = {1};
     if (a == 0 and b == 0) {
       break;
     }
-    unordered_map<int, int> A, B;
-    int sa = 0, sb = 0;
-    A[a] = sa++;
-    B[b] = sb++;
-    int na = a, nb = b;
-    int meet = 1;
-    while (true) {
-      if (A.count(nb) or B.count(na)) {
-        meet = A.count(nb) ? nb : na;
-        break;
-      }
-      if (na == 1 and nb == 1) {
-        break;
-      }
-      if (na != 1) {
-        na = (na % 2) ? 3 * na + 1 : na / 2;
-        if (!A.count(na)) {
-          A[na] = sa;
-        }
-        sa++;
-      }
-      if (nb != 1) {
-        nb = (nb % 2) ? 3 * nb + 1 : nb / 2;
-        if (!B.count(nb)) {
-          B[nb] = sb;
-        }
-        sb++;
-      }
-    }
-    cout << a << " needs " << A[meet] << " steps, ";
-    cout << b << " needs " << B[meet] << " steps, ";
-    cout << "they meet at " << meet << "\n";
+    int ca = populate(a, 0);
+    ntoc[a] = ca;
+    cton[ca].push_back(a);
+    meet(cton[ca]);
+    int cb = populate(b, 0);
+    ntoc[b] = cb;
+    cton[cb].push_back(b);
+    meet(cton[cb]);
+    sort(at.rbegin(), at.rend());
+    cout << at.size() << "\n";
+    /*
+    int dest = at[0];
+    cout << a << " needs " << ntoc[a] - ntoc[dest] << " steps, ";
+    cout << b << " needs " << ntoc[b] - ntoc[dest] << " steps, ";
+    cout << "they meet at " << dest << "\n";
+    ntoc.clear();
+    cton.clear();
+    at.clear();
+    */
   }
   return 0;
 }
