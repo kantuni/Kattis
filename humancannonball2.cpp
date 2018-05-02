1e82a9df02994715893a30eed15566940d2341d6
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-05-01 21:28:18 -0400

    Update humancannonball2.cpp

--- humancannonball2.cpp
+++ humancannonball2.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-  const double EPS = 1e-10;
   const double PI = acos(-1.0);
   const double g = 9.81;
+  const double EPS = 1e-10;
   int n;
   cin >> n;
   while (n--) {
@@ -13,9 +13,9 @@ int main() {
     t = x1 / (v0 * cos(theta * PI / 180.0));
     y = x1 * tan(theta * PI / 180.0) - 0.5 * g * t * t;
     if (y - h1 - 1 > EPS and h2 - y - 1 > EPS) {
-      cout << "Safe\n";
+      cout << "Safe" << "\n";
     } else {
-      cout << "Not Safe\n";
+      cout << "Not Safe" << "\n";
     }
   }
   return 0;
