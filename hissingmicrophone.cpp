196c4572895db835b398e05a9620e26aa1b5a11a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-05-01 21:27:19 -0400

    Update hissingmicrophone.cpp

--- hissingmicrophone.cpp
+++ hissingmicrophone.cpp
@@ -12,9 +12,9 @@ int main() {
     }
   }
   if (hiss) {
-    cout << "hiss\n";
+    cout << "hiss" << "\n";
   } else {
-    cout << "no hiss\n";
+    cout << "no hiss" << "\n";
   }
   return 0;
 }
