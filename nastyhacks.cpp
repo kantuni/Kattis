e2fceb05715eee9a72de2f646aa7fabce21f65a4
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2018-05-06 09:40:42 -0400

    Update nastyhacks.cpp

--- nastyhacks.cpp
+++ nastyhacks.cpp
@@ -8,11 +8,11 @@ int main() {
     int r, e, c;
     cin >> r >> e >> c;
     if (e - c > r) {
-      cout << "advertise\n";
+      cout << "advertise" << "\n";
     } else if (e - c < r) {
-      cout << "do not advertise\n";
+      cout << "do not advertise" << "\n";
     } else {
-      cout << "does not matter\n";
+      cout << "does not matter" << "\n";
     }
   }
   return 0;
