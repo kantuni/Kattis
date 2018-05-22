d2b4502212ee7b01802b80cf33d12ea98580307c
kantuni <henrikh.kantuni@gmail.com>
2018-05-22 01:00:11 -0400

    A new hope

--- collatz.cpp
+++ collatz.cpp
@@ -1,7 +1,20 @@
+// 1. Build a binary tree
+// 2. Find LCA for each query
+
 #include <bits/stdc++.h>
 using namespace std;
 
+struct Node {
+  unsigned long long value;
+  Node *left, *right;
+};
+
 int main() {
-  
+  int a, b;
+  while (cin >> a >> b) {
+    if (a == 0 and b == 0) {
+      break;
+    }
+  }
   return 0;
 }
