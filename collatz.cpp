ca8b81499b265a03a94e8bee33ebfea14f468338
kantuni <henrikh.kantuni@gmail.com>
2018-05-22 22:42:48 -0400

    Finally

--- collatz.cpp
+++ collatz.cpp
@@ -1,33 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+unordered_map<long long, long long> fromA;
+long long C;
+
+void Ato1(long long A) {
+  fromA[A] = 0;
+  long long cnt = 0;
+  while (A > 1) {
+    A = (A % 2) ? 3 * A + 1 : A / 2;
+    cnt++;
+    fromA[A] = cnt;
+  }
+}
+
+long long BtoA(long long B) {
+  if (fromA.count(B)) {
+    C = B;
+    return 0;
+  }
+  long long cnt = 0;
+  while (B > 1) {
+    B = (B % 2) ? 3 * B + 1 : B / 2;
+    cnt++;
+    if (fromA.count(B)) {
+      C = B;
+      return cnt;
+    }
+  }
+  C = 1;
+  return cnt;
+}
+
 int main() {
-  int a, b;
-  while (cin >> a >> b) {
-    if (a == 0 and b == 0) {
+  long long A, B;
+  while (cin >> A >> B) {
+    if (A == 0 and B == 0) {
       break;
     }
-    unordered_map<int, int> memo;
-    int meet = 1;
-    int ac = a, acnt = 0;
-    while (ac != 1) {
-      memo[ac] = acnt;
-      ac = (ac % 2) ? 3 * ac + 1 : ac / 2;
-      acnt++;
-    }
-    int bc = b, bcnt = 0;
-    while (bc != 1) {
-      if (memo.count(bc)) {
-        meet = bc;
-        break;
-      }
-      bc = (bc % 2) ? 3 * bc + 1 : bc / 2;
-      bcnt++;
-    }
-    int sa = memo[meet], sb = bcnt;
-    cout << a << " needs " << sa << " steps, ";
-    cout << b << " needs " << sb << " steps, ";
-    cout << "they meet at " << meet << "\n";
+    Ato1(A);                     // (1)
+    long long Sb = BtoA(B);      // (2)
+    long long Sa = fromA[C];     // (3)
+    cout << A << " needs " << Sa << " steps, ";
+    cout << B << " needs " << Sb << " steps, ";
+    cout << "they meet at " << C << "\n";
+    fromA.clear();
   }
   return 0;
 }
