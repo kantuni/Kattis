2f7f7e81472fe9d354467b3391b5d54561ac3d78
kantuni <henrikh.kantuni@gmail.com>
2018-05-22 20:15:36 -0400

    TLE again and again

--- collatz.cpp
+++ collatz.cpp
@@ -1,13 +1,22 @@
-// 1. Build a binary tree
-// 2. Find LCA for each query
-
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
-  unsigned long long value;
-  Node *left, *right;
-};
+map<int, int> memo;
+
+int meet(int a, int b) {
+  set<int> visited;
+  while (a != 1) {
+    visited.insert(a);
+    a = memo[a];
+  }
+  while (b != 1) {
+    if (visited.count(b)) {
+      return b;
+    }
+    b = memo[b];
+  }
+  return 1;
+}
 
 int main() {
   int a, b;
@@ -15,6 +24,44 @@ int main() {
     if (a == 0 and b == 0) {
       break;
     }
+    int ac = a;
+    while (ac != 1 and !memo.count(ac)) {
+      int next;
+      if (ac % 2) {
+        next = 3 * ac + 1;
+      } else {
+        next = ac / 2;
+      }
+      memo[ac] = next;
+      ac = next;
+    }
+    int bc = b;
+    while (bc != 1 and !memo.count(bc)) {
+      int next;
+      if (bc % 2) {
+        next = 3 * bc + 1;
+      } else {
+        next = bc / 2;
+      }
+      memo[bc] = next;
+      bc = next;
+    }
+    int c = meet(a, b);
+    cout << a << " needs ";
+    int sa = 0;
+    while (a != c) {
+      a = memo[a];
+      sa++;
+    }
+    cout << sa << " steps, ";
+    cout << b << " needs ";
+    int sb = 0;
+    while (b != c) {
+      b = memo[b];
+      sb++;
+    }
+    cout << sb << " steps, ";
+    cout << "they meet at " << c << "\n";
   }
   return 0;
 }
