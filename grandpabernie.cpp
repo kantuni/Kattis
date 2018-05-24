ecf0578f4335a8b414ec05171705aff707a659d5
kantuni <henrikh.kantuni@gmail.com>
2018-05-24 12:08:31 -0400

    TLE again

--- grandpabernie.cpp
+++ grandpabernie.cpp
@@ -2,23 +2,26 @@
 using namespace std;
 
 int main() {
-  unordered_map<string, multiset<int>> memo;
+  unordered_map<string, vector<int>> memo;
   int n;
   cin >> n;
   while (n--) {
     string s;
     int y;
     cin >> s >> y;
-    memo[s].insert(y);
+    memo[s].push_back(y);
   }
+  unordered_set<string> visited;
   int q;
   cin >> q;
   while (q--) {
     string s;
     int k;
     cin >> s >> k;
-    auto it = next(memo[s].begin(), k - 1);
-    cout << *it << "\n";
+    if (!visited.count(s)) {
+      sort(memo[s].begin(), memo[s].end());
+    }
+    cout << memo[s][k - 1] << "\n";
   }
   return 0;
 }
