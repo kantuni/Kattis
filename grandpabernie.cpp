1b66a16d5fa9c9d4c1551ae2d024202e131b1c56
kantuni <henrikh.kantuni@gmail.com>
2018-05-24 12:23:32 -0400

    Will need a custom BST here

--- grandpabernie.cpp
+++ grandpabernie.cpp
@@ -2,26 +2,19 @@
 using namespace std;
 
 int main() {
-  unordered_map<string, vector<int>> memo;
   int n;
   cin >> n;
   while (n--) {
     string s;
     int y;
     cin >> s >> y;
-    memo[s].push_back(y);
   }
-  unordered_set<string> visited;
   int q;
   cin >> q;
   while (q--) {
     string s;
     int k;
     cin >> s >> k;
-    if (!visited.count(s)) {
-      sort(memo[s].begin(), memo[s].end());
-    }
-    cout << memo[s][k - 1] << "\n";
   }
   return 0;
 }
