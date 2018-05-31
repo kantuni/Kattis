fb39a965e290edf5406227c97a1af5a0b1a44f2c
kantuni <henrikh.kantuni@gmail.com>
2018-05-31 17:23:52 +0400

    Grandpa Bernie - RTE

--- grandpabernie.cpp
+++ grandpabernie.cpp
@@ -5,71 +5,119 @@ struct Node {
   int x, y, size;
   Node *left, *right;
 
-  Node(int x, Node *left = NULL, Node *right = NULL) {
+  Node(int x) {
     this->x = x;
     this->y = rand();
     this->size = 1;
-    this->left = left;
-    this->right = right;
-  }
-
-  void recalc() {
-    int l = (this->left != NULL) ? this->left->size : 0;
-    int r = (this->right != NULL) ? this->right->size : 0;
-    this->size = l + r + 1;
-  }
-
-  Node* split(int x, Node *T) {
-    if (T == NULL) return NULL;
-    Node *root;
-    if (x < T->x) {
-      Node *S = split(x, T->left);
-      Node *SL = (S != NULL) ? S->left : NULL;
-      Node *SR = (S != NULL) ? S->right : NULL;
-      T->left = SR;
-      root = new Node(x, SL, T);
-    } else {
-      Node *S = split(x, T->right);
-      Node *SL = (S != NULL) ? S->left : NULL;
-      Node *SR = (S != NULL) ? S->right : NULL;
-      T->right = SL;
-      root = new Node(x, T, SR);
-    }
-    root->recalc();
-    return root;
-  }
-
-  Node* insert(int x) {
-    return split(x, this);
-  }
-
-  int at(int k) {
-    int m = (this->left != NULL) ? this->left->size : 0;
-    if (k == m + 1) {
-      return this->x;
-    } else if (k <= m) {
-       return this->left->at(k);
-    } else {
-      return this->right->at(k - m - 1);
-    }
+    this->left = this->right = NULL;
   }
 };
 
+class Treap {
+  Node *root;
+
+  public:
+    Treap() {
+      this->root = NULL;
+    }
+
+    int size(Node* node) {
+      return (node != NULL) ? node->size : 0;
+    }
+
+    void insert(int x) {
+      insert(this->root, x);
+    }
+
+    int at(int k) {
+      return at(this->root, k);
+    }
+
+    void print() {
+      print(this->root);
+    }
+
+  private:
+    void insert(Node* &node, int x) {
+      if (node == NULL) {
+        node = new Node(x);
+        return;
+      }
+      if (x < node->x) {
+        insert(node->left, x);
+        if (node->y < node->left->y) {
+          node = rr(node);
+        }
+      } else {
+        insert(node->right, x);
+        if (node->y < node->right->y) {
+          node = lr(node);
+        }
+      }
+      node->size += 1;
+    }
+
+    Node* lr(Node* node) {
+      Node *r = node->right, *rl = node->right->left;
+      if (r != NULL) {
+        r->left = node;
+      }
+      node->right = rl;
+      return r;
+    }
+
+    Node* rr(Node* node) {
+      Node *l = node->left, *lr = node->left->right;
+      if (l != NULL) {
+        l->right = node;
+      }
+      node->left = lr;
+      return l;
+    }
+
+    int at(Node* node, int k) {
+      int m = (node->left != NULL) ? node->left->size : 0;
+      if (k == m + 1) {
+        return node->x;
+      } else if (k > m) {
+        return at(node->right, k - m - 1);
+      } else {
+        return at(node->left, k);
+      }
+    }
+
+    void print(Node* node) {
+      if (node->left != NULL) {
+        print(node->left);
+      }
+      cout << node->x << "," << node->y << "," << node->size << " ";
+      if (node->right != NULL) {
+        print(node->right);
+      }
+    }
+};
+
 int main() {
-  srand(0);
   int n;
   cin >> n;
-  unordered_map<string, Node*> memo;
+  unordered_map<string, Treap*> memo;
   while (n--) {
     string s;
-    int y;
-    cin >> s >> y;
+    int year;
+    cin >> s >> year;
     if (memo[s] == NULL) {
-      memo[s] = new Node(y);
-    } else {
-      memo[s] = memo[s]->insert(y);
+      memo[s] = new Treap();
     }
+    memo[s]->insert(year);
   }
+  /*
+  for (auto m: memo) {
+    cout << m.first << ": \n";
+    m.second->print();
+    cout << "\n";
+  }
+  cout << "\n";
+  */
   int q;
   cin >> q;
   while (q--) {
