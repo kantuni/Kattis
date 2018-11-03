f7e2986f8fbc84a76adaada413a29f8a20cc9fba
kantuni <henrikh.kantuni@gmail.com>
2018-11-02 23:51:46 -0400

    Run-Length Encoding, Run

---
+++ runlengthencodingrun.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string encode(string m) {
+  string c;
+  for (int i = 0; i < m.size(); i++) {
+    int cnt = 1;
+    for (int j = i + 1; j < m.size(); j++) {
+      if (m[j] == m[i]) {
+        cnt++;
+      } else {
+        break;
+      }
+    }
+    c += m[i] + to_string(cnt);
+    i += cnt - 1;
+  }
+  return c;
+}
+
+string decode(string c) {
+  string m;
+  for (int i = 0; i < c.size(); i++) {
+    if (isdigit(c[i])) {
+      int cnt = c[i] - '0';
+      m += string(cnt - 1, c[i - 1]);
+    } else {
+      m += c[i];
+    }
+  }
+  return m;
+}
+
+int main() {
+  string action, s;
+  cin >> action >> s;
+  if (action == "E") {
+    cout << encode(s) << "\n";
+  } else {
+    cout << decode(s) << "\n";
+  }
+  return 0;
+}
