eee56c641e286530765e228b79c9523cba7b0e24
kantuni <henrikh.kantuni@gmail.com>
2018-11-03 10:32:46 -0400

    Flying Safely

---
+++ flyingsafely.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, m;
+    cin >> n >> m;
+    while (m--) {
+      int a, b;
+      cin >> a >> b;
+    }
+    cout << n - 1 << "\n";
+  }
+  return 0;
+}
