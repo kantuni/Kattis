3cf089a15abf5e888c8ecd794972432624c8e1bb
kantuni <henrikh.kantuni@gmail.com>
2018-11-02 23:00:33 -0400

    Just a Minute

---
+++ justaminute.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  int num = 0, denom = 0;
+  for (int i = 0; i < n; i++) {
+    int m, s;
+    cin >> m >> s;
+    num += s - m * 60;
+    denom += m * 60;
+  }
+  double ans = 1. * (num + denom) / denom;
+  if (ans > 1) {
+    cout << fixed << setprecision(8);
+    cout << ans << "\n";
+  } else {
+    cout << "measurement error" << "\n";
+  }
+  return 0;
+}
