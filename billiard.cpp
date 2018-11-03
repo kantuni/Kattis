3d2cdad357929447a4c3cdf53eba5a9c7d0e8c8a
kantuni <henrikh.kantuni@gmail.com>
2018-11-03 17:40:29 -0400

    Update billiard.cpp

--- billiard.cpp
+++ billiard.cpp
@@ -1,28 +1,18 @@
 #include <bits/stdc++.h>
 #define PI acos(-1.0)
-#define EPS 1e-10
 using namespace std;
 
 int main() {
-  double a, b, s, m, n;
+  int a, b, s, m, n;
   while (true) {
     cin >> a >> b >> s >> m >> n;
     if (a + b + s + m + n == 0) {
       break;
     }
-    double A, v, x, y;
-    double low = 0, high = 90;
-    while (high - low > EPS) {
-      A = (low + high) / 2;
-      x = b / (2 * sin(A * PI / 180));
-      y = a / (2 * cos(A * PI / 180));
-      if (x * n > y * m) {
-        low = A;
-      } else {
-        high = A;
-      }
-    }
-    v = (x * n + y * m) / s;
+    double x = a * m;
+    double y = b * n;
+    double A = atan(y / x) * 180 / PI;
+    double v = hypot(x, y) / s;
     cout << fixed << setprecision(2);
     cout << A << " " << v << "\n";
   }
