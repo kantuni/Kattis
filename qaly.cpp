644da2758d887579db33655e9e8b955de20653c4
kantuni <henrikh.kantuni@gmail.com>
2018-11-05 09:39:47 -0500

    Quality-Adjusted Life-Year

---
+++ qaly.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  double ans = 0;
+  while (n--) {
+    double q, y;
+    cin >> q >> y;
+    ans += q * y;
+  }
+  cout << ans << "\n";
+  return 0;
+}
