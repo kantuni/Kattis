8e57805cfa134ce144bf11545e9ce0763417658f
kantuni <henrikh.kantuni@gmail.com>
2018-11-06 12:07:59 -0500

    Exam

---
+++ exam.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int k;
+  cin >> k;
+  string me, you;
+  cin >> me >> you;
+  int same = 0, diff = 0;
+  for (int i = 0; i < me.size(); i++) {
+    if (me[i] == you[i]) {
+      same++;
+    } else {
+      diff++;
+    }
+  }
+  int ans = (k > same) ? me.size() - (k - same) : k + diff;
+  cout << ans << "\n";
+  return 0;
+}
