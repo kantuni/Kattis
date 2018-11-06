e7e1944dd087f3b84ce9624555098f3e504185e6
kantuni <henrikh.kantuni@gmail.com>
2018-11-06 11:20:47 -0500

    Euler's Number

---
+++ eulersnumber.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  long double e = 1, f = 1;
+  for (int i = 1; i < n + 1; i++) {
+    f *= i;
+    e += 1.0 / f;
+  }
+  cout << fixed << setprecision(15);
+  cout << e << "\n";
+  return 0;
+}
