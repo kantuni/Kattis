396af3fbb9ff0857d8fbf3519a4c3efdbfd865d3
kantuni <henrikh.kantuni@gmail.com>
2018-12-01 23:14:38 -0500

    Relocation

---
+++ relocation.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, q;
+  cin >> n >> q;
+  int loc[n];
+  for (int i = 0; i < n; i++) {
+    cin >> loc[i];
+  }
+  while (q--) {
+    int t;
+    cin >> t;
+    if (t == 1) {
+      int c, x;
+      cin >> c >> x;
+      loc[c - 1] = x;
+    } else {
+      int a, b;
+      cin >> a >> b;
+      int ans = abs(loc[a - 1] - loc[b - 1]);
+      cout << ans << "\n"; 
+    }
+  }
+  return 0;
+}
