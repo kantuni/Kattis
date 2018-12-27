74b4655ed908c27ce21f443d58e57090bec93cc3
kantuni <henrikh.kantuni@gmail.com>
2018-12-28 01:53:48 +0400

    Free Food

---
+++ freefood.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> d(365, 0);
+  while (n--) {
+    int s, t;
+    cin >> s >> t;
+    for (int i = s - 1; i < t; i++) {
+      d[i] = 1;
+    }
+  }
+  int ans = 0;
+  for (int i = 0; i < d.size(); i++) {
+    ans += d[i];
+  }
+  cout << ans << "\n";
+  return 0;
+}
