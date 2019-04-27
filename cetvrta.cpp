96f2f5860acd8cbdd9e4d052c7d1fb75a6497c8a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-04-27 18:24:09 +0400

    Update cetvrta.cpp

--- cetvrta.cpp
+++ cetvrta.cpp
@@ -6,7 +6,7 @@ int main() {
   for (int i = 0; i < 3; i++) {
     int x, y;
     cin >> x >> y;
-    coords.push_back(make_pair(x, y));
+    coords.push_back({x, y});
   }
   // (x1, y1), (x1, y2), (x2, y1), (x2, y2)
   // where x1 ≥ x2, y1 ≥ y2
@@ -17,19 +17,19 @@ int main() {
     dx = coords[2].first - coords[0].first;
     dy = coords[1].second - coords[0].second;
     if (coords[2].second == coords[0].second) {
-      ans = make_pair(coords[0].first + dx, coords[0].second + dy);
+      ans = {coords[0].first + dx, coords[0].second + dy};
     } else {
-      ans = make_pair(coords[0].first + dx, coords[0].second);
+      ans = {coords[0].first + dx, coords[0].second};
     }
   } else {
     dx = coords[2].first - coords[0].first;
     dy = coords[2].second - coords[1].second;
     if (coords[2].second == coords[0].second) {
-      ans = make_pair(coords[2].first - dx, coords[2].second - dy);
+      ans = {coords[2].first - dx, coords[2].second - dy};
     } else {
-      ans = make_pair(coords[2].first - dx, coords[2].second);
+      ans = {coords[2].first - dx, coords[2].second};
     }
   }
-  cout << ans.first << " " << ans.second << "\n";
+  cout << ans.first << " " << ans.second << endl;
   return 0;
 }
