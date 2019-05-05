983a8893ca0b5839e976ee9290463deb1276bbdb
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-05-06 01:36:30 +0400

    Update encodedmessage.cpp

--- encodedmessage.cpp
+++ encodedmessage.cpp
@@ -2,16 +2,15 @@
 using namespace std;
 
 string decode(string s) {
-  int n = sqrt(s.size());
-  vector<vector<char>> square(n, vector<char>(n));
-  int next = 0;
+  int n = sqrt(s.size()), next = 0;
+  char square[n][n];
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       square[i][j] = s[next++];
     }
   }
   string m = "";
-  for (int j = n - 1; j >= 0; j--) {
+  for (int j = n - 1; j > -1; j--) {
     for (int i = 0; i < n; i++) {
       m += string(1, square[i][j]);
     }
@@ -25,7 +24,7 @@ int main() {
   while (t--) {
     string s;
     cin >> s;
-    cout << decode(s) << "\n";
+    cout << decode(s) << endl;
   }
   return 0;
 }
