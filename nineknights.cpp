27a8c4e5feb48f3c1d69c53de77a55875edd1646
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-08-23 01:14:23 -0400

    Update nineknights.cpp

--- nineknights.cpp
+++ nineknights.cpp
@@ -3,25 +3,25 @@ using namespace std;
 
 int main() {
   char B[5][5];
-  int knights = 0;
-  for (int i = 0; i < 5; i++) {
-    for (int j = 0; j < 5; j++) {
-      cin >> B[i][j];
-      if (B[i][j] == 'k') {
-        knights++;
+  int k = 0;
+  for (int r = 0; r < 5; r++) {
+    for (int c = 0; c < 5; c++) {
+      cin >> B[r][c];
+      if (B[r][c] == 'k') {
+        k++;
       }
     }
   }
   char dr[8] = {-1, -2, -2, -1, 1, 2, 2, 1};
   char dc[8] = {-2, -1, 1, 2, 2, 1, -1, -2};
-  bool valid = true;
-  for (int r = 0; r < 5; r++) {
-    for (int c = 0; c < 5; c++) {
+  bool valid = k == 9;
+  for (int r = 0; valid and r < 5; r++) {
+    for (int c = 0; valid and c < 5; c++) {
       if (B[r][c] == 'k') {
         for (int i = 0; i < 8; i++) {
-          bool ok = r + dr[i] >= 0 and r + dr[i] < 5;
-          ok = ok and c + dc[i] >= 0 and c + dc[i] < 5;
-          if (ok and B[r + dr[i]][c + dc[i]] == 'k') {
+          bool rok = r + dr[i] >= 0 and r + dr[i] < 5;
+          bool cok = c + dc[i] >= 0 and c + dc[i] < 5;
+          if (rok and cok and B[r + dr[i]][c + dc[i]] == 'k') {
             valid = false;
             break;
           }
@@ -29,10 +29,10 @@ int main() {
       }
     }
   }
-  if (valid and knights == 9) {
-    cout << "valid" << "\n";
+  if (valid) {
+    cout << "valid" << endl;
   } else {
-    cout << "invalid" << "\n";
+    cout << "invalid" << endl;
   }
   return 0;
 }
