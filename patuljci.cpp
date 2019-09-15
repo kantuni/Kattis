e9c4c637cd78aaa4e1bfdfe7757f8b935afcbdc6
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-15 17:35:36 -0400

    Update patuljci.cpp

--- patuljci.cpp
+++ patuljci.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 int main() {
-  vector<int> dwarves(9);
+  vector<int> d(9);
   int sum = 0;
   for (int i = 0; i < 9; i++) {
-    cin >> dwarves[i];
-    sum += dwarves[i];
+    cin >> d[i];
+    sum += d[i];
   }
   for (int i = 0; i < 8; i++) {
     for (int j = i + 1; j < 9; j++) {
-      if (dwarves[i] + dwarves[j] == sum - 100) {
-        dwarves[i] = 0;
-        dwarves[j] = 0;
+      if (d[i] + d[j] == sum - 100) {
+        d[i] = 0;
+        d[j] = 0;
       }
     }
   }
   for (int i = 0; i < 9; i++) {
-    if (dwarves[i] != 0) {
-      cout << dwarves[i] << "\n";
+    if (d[i] != 0) {
+      cout << d[i] << endl;
     }
   }
   return 0;
