09f650a93c0f8390263eb48ae95b4de237c5edf9
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-26 10:40:07 +0400

    Update romans.cpp

--- romans.cpp
+++ romans.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
   float x;
   cin >> x;
-  int ans = x * (5280.0 / 4854.0) * 1000. + 0.5;
+  int ans = x * (5280.0 / 4854.0) * 1000.0 + 0.5;
   cout << ans << endl;
   return 0;
 }
