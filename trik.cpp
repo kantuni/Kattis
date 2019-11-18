9cbe890f3691f298ca6eea3fe3d47877bc33f19a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-11-18 23:42:08 +0400

    Update trik.cpp

--- trik.cpp
+++ trik.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() { 
   string moves;
   cin >> moves;
-  int cups[3] = {1, 0, 0};
+  vector<int> cups = {1, 0, 0};
   for (int i = 0; i < moves.size(); i++) {
     if (moves[i] == 'A') {
       swap(cups[0], cups[1]);
