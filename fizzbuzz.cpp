b00121ac755cf556d6fdc1a3ecc4c1bbde84b977
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-06-16 17:47:15 +0400

    Update fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -6,13 +6,13 @@ int main() {
   cin >> x >> y >> n;
   for (int i = 1; i < n + 1; i++) {
     if (i % x == 0 and i % y == 0) {
-      cout << "FizzBuzz" << "\n";
+      cout << "FizzBuzz" << endl;
     } else if (i % x == 0) {
-      cout << "Fizz" << "\n";
+      cout << "Fizz" << endl;
     } else if (i % y == 0) {
-      cout << "Buzz" << "\n";
+      cout << "Buzz" << endl;
     } else {
-      cout << i << "\n";
+      cout << i << endl;
     }
   }
   return 0;
