8db2b82b82b3b48b4433abb3a125b06956cd9083
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-06-22 14:14:16 +0400

    Update mirror.cpp

--- mirror.cpp
+++ mirror.cpp
@@ -7,18 +7,18 @@ int main() {
   for (int t = 1; t < T + 1; t++) {
     int r, c;
     cin >> r >> c;
-    vector<vector<char>> M(r, vector<char>(c));
+    char M[r][c];
     for (int i = 0; i < r; i++) {
       for (int j = 0; j < c; j++) {
         cin >> M[i][j];
       }
     }
-    cout << "Test " << t << "\n";
+    cout << "Test " << t << endl;
     for (int i = 0; i < r; i++) {
       for (int j = 0; j < c; j++) {
         cout << M[r - 1 - i][c - 1 - j];
       }
-      cout << "\n";
+      cout << endl;
     }
   }
   return 0;
